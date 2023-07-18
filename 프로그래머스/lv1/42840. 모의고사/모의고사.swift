76516184a5169f89ef637d2c46c952744a3d8454
Lee Yuri <leeyuri.iosapp@gmail.com>
2023-07-18 16:02:11 +0900

    [level 1] Title: 모의고사, Time: 2.20 ms, Memory: 17.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+func solution(_ answers:[Int]) -> [Int] {
+    var answer:[Int] = []
+    var maxValue = 0
+    
+    var firstArr = Array(repeating: [1, 2, 3, 4, 5], count:10000/5).flatMap { $0 }
+    var secondArr = Array(repeating: [2, 1, 2, 3, 2, 4, 2, 5], count:10000/8).flatMap { $0 }
+    var thirdArr = Array(repeating: [3, 3, 1, 1, 2, 2, 4, 4, 5, 5], count:10000/10).flatMap { $0 }
+    
+    var first = 0, second = 0, third = 0
+    
+    for i in 0..<answers.count {
+        if firstArr[i] == answers[i] {
+            first += 1
+        }
+        
+        if secondArr[i] == answers[i] {
+            second += 1
+        }
+        
+        if thirdArr[i] == answers[i] {
+            third += 1
+        }
+    }
+    
+    maxValue = [first, second, third].sorted(by: >)[0]
+    
+    return [(1, first), (2, second), (3, third)].filter { $0.1 == maxValue }.map { $0.0 }
+}
