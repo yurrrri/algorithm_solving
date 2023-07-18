7efe4af788780286e456489dbf47b15f397e66d9
Lee Yuri <leeyuri.iosapp@gmail.com>
2023-07-18 16:10:05 +0900

    [level 1] Title: 모의고사, Time: 2.06 ms, Memory: 16.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.swift
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.swift
@@ -4,6 +4,7 @@ func solution(_ answers:[Int]) -> [Int] {
     var answer:[Int] = []
     var maxValue = 0
     
+    // 노트1. repeating을 배열 기준으로 할 수 있다.
     var firstArr = Array(repeating: [1, 2, 3, 4, 5], count:10000/5).flatMap { $0 }
     var secondArr = Array(repeating: [2, 1, 2, 3, 2, 4, 2, 5], count:10000/8).flatMap { $0 }
     var thirdArr = Array(repeating: [3, 3, 1, 1, 2, 2, 4, 4, 5, 5], count:10000/10).flatMap { $0 }
@@ -24,7 +25,7 @@ func solution(_ answers:[Int]) -> [Int] {
         }
     }
     
-    maxValue = [first, second, third].sorted(by: >)[0]
+    maxValue = [first, second, third].max()!
     
     return [(1, first), (2, second), (3, third)].filter { $0.1 == maxValue }.map { $0.0 }
 }
