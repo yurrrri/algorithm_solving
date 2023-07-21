5ea895425bac8f727656d416a433afd5b9dc0fc2
Lee Yuri <leeyuri.iosapp@gmail.com>
2023-07-21 15:26:15 +0900

    [level 2] Title: 전화번호 목록, Time: 325.01 ms, Memory: 34.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,8 +1,19 @@
 def solution(phone_book):
-    phone_book.sort()
+#     phone_book.sort()
     
-    for i in range(len(phone_book)-1):
-        if phone_book[i+1].startswith(phone_book[i]):
-            return False
+#     for i in range(len(phone_book)-1):
+#         if phone_book[i+1].startswith(phone_book[i]):
+#             return False
     
+#     return True
+    dic = {}
+    
+    for phone_number in phone_book:
+        dic[phone_number] = 1
+    for phone_number in phone_book:
+        temp = ""
+        for number in phone_number:
+            temp += number
+            if temp in dic and temp != phone_number:
+                return False
     return True
