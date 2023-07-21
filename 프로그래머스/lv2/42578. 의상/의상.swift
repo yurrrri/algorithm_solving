3832ddb9c3affe0664d6f5294c4bd54b23f7b5b4
Lee Yuri <leeyuri.iosapp@gmail.com>
2023-07-21 16:46:44 +0900

    [level 2] Title: 의상, Time: 0.08 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.swift
+++ 프로그래머스/lv2/42578. 의상/의상.swift
@@ -9,7 +9,7 @@ func solution(_ clothes:[[String]]) -> Int {
     }
     
     for (k, v) in dic {
-        answer *= (v+1)
+        answer *= (v+1) // 해당 카테고리에서 하나도 선택하지 않는 경우까지 포함하여 곱함
     }
-    return answer-1
+    return answer-1  // (모자1, 안경1), (모자2, 안경1), (모자1, X), (모자2, X), (안경1, X), (X, X) 라는 경우의 수가 생기므로 여기서 하나도 입지 않는 (X, X)의 개수=1 을 빼면 정답
 }
