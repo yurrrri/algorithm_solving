11f107feddc9ac5055a2ac8b46a1d7e8fac9852e
Lee Yuri <leeyuri.iosapp@gmail.com>
2023-07-24 12:43:59 +0900

    [unrated] Title: 접두사인지 확인하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.py
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.py
@@ -1,2 +1,3 @@
 def solution(my_string, is_prefix):
-    return 1 if my_string[:len(is_prefix)] == is_prefix else 0
+    # return 1 if my_string[:len(is_prefix)] == is_prefix else 0
+    return 1 if my_string.startswith(is_prefix) else 0
