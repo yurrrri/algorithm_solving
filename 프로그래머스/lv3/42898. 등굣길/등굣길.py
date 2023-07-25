9b103f749a46eef4ff6ac8ecfd88ad5fdcbf550b
Lee Yuri <leeyuri.iosapp@gmail.com>
2023-07-25 13:07:56 +0900

    [level 3] Title: 등굣길, Time: 2.07 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42898. 등굣길/등굣길.py
@@ -0,0 +1,14 @@
+def solution(m, n, puddles):
+    dp = [[0] * (m+1) for _ in range(n+1)] # n*m 배열 생성
+    dp[1][1] = 1
+    
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if i == 1 and j == 1:
+                continue
+            if [j, i] in puddles:
+                dp[i][j] = 0
+                continue
+            dp[i][j] = dp[i-1][j] + dp[i][j-1]
+    
+    return (dp[n][m]) % 1000000007
