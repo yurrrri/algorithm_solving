fc0b11686d93afee21a6132242675d9defa50775
Lee Yuri <leeyuri.iosapp@gmail.com>
2023-07-25 11:39:39 +0900

    [level 3] Title: N으로 표현, Time: 217.54 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/lv3/42895. N으로 표현/N으로 표현.swift
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.swift
@@ -5,6 +5,9 @@ func solution(_ n:Int, _ number:Int) -> Int {
     var temp = 0
     
     func dfs(_ result: Int, _ count: Int) {
+        //오답노트 2. 아래를 안쓰면 core dumped 에러가 뜨는이유?
+        //1. recursive 오류일수도 있고,
+        //2. 제한을 안걸어주면 result값이 계속 커지는데 이게 정수의 범위를 넘어서서 발생하는 현상일수도 있음
         if count >= 9 {
             return
         }
@@ -14,6 +17,8 @@ func solution(_ n:Int, _ number:Int) -> Int {
             return
         }
         
+        //오답노트 1. 재귀를 들어갈 떄마다 temp를 0으로 초기화하는 이유?
+        // 그 다음 연산으로 들어갈 때마다 다시 5, 55, 555... 순으로 사칙연산 경우의 수를 따져줘야하니, 기존의 값을 쓰는것이 아닌 다시 처음부터 시작해야함
         var temp = 0
         for i in 1...9 {
             temp = 10*temp + n // 5, 55, 555, 55555.....
