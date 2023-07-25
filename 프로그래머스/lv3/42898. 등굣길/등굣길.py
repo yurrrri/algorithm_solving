8cf580433518232c46f9a281e3f8cb5918132563
Lee Yuri <leeyuri.iosapp@gmail.com>
2023-07-25 13:22:00 +0900

    [level 3] Title: 등굣길, Time: 2.24 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/42898. 등굣길/등굣길.py
+++ 프로그래머스/lv3/42898. 등굣길/등굣길.py
@@ -6,9 +6,19 @@ def solution(m, n, puddles):
         for j in range(1, m+1):
             if i == 1 and j == 1:
                 continue
-            if [j, i] in puddles:
-                dp[i][j] = 0
+            if [j, i] in puddles: # 오답노트 1. 물웅덩이는 1개 이상임 -> 일치가 아닌 포함인지를 따져야함
+                                  # 문제에서 주어진 좌표와 코딩할때의 배열 좌표가 서로 역전되어있음을 주의해야함
+                                  # 문제에서 학교위치가 (m, n)이라고 나와있음
                 continue
-            dp[i][j] = dp[i-1][j] + dp[i][j-1]
-    
-    return (dp[n][m]) % 1000000007
+            
+            if i == 1:
+                dp[i][j] = dp[i][j-1]
+            elif j == 1:
+                dp[i][j] = dp[i-1][j]
+            else:
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+            
+    # DFS로 푸는 방법(재귀)
+            
+
+    return dp[n][m] % 1000000007
