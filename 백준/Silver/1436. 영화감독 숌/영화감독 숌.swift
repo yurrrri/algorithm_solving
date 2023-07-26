1f7f3a87b8797ad0d905a6410bfe05fc8dcf5f66
Lee Yuri <leeyuri.iosapp@gmail.com>
2023-07-26 18:47:35 +0900

    [Silver V] Title: 영화감독 숌, Time: 40 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.swift
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.swift
@@ -1,32 +1,19 @@
-import Foundation
+var n = Int(readLine()!)!, count = 0, x = 0
 
-let n = Int(readLine()!)!
-var arr:[Int] = []
-var count = 0
-var num = 666
-
-while true {
-  var c = 0
-  for i in String(num) {
-    if i == "6" {
-      c+=1
-
-      if c == 3 {
-        break
-      }
-    } else {
-      c = 0
+for i in 1...Int.max {
+    x = i
+    while x > 0 { // 각 i마다 1000으로 나누었을 때 나머지가 666이라면 666을 포함하는것임
+      // String 형 변환 안하고 666 확인하는 방법
+        if x % 1000 == 666 {
+          // 오답노트1. 1000으로 나누는 이유
+          // 666 -> 나눴을 때 3자리수만 남겨서 확인하기 위함
+            count += 1
+            break
+        }
+        x /= 10
     }
-  }
-
-  if c == 3 {
-    count += 1
-  }
-
-  if count == n {
-    print(num)
-    break
-  }
-
-  num += 1
+    if count == n {
+        print(i)
+        break
+    }
 }
