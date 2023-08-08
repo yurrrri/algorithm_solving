c7f557829fbc985aa1f05efe06c10fda98237341
Lee Yuri <leeyuri.iosapp@gmail.com>
2023-08-08 18:27:54 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
@@ -1,15 +1,25 @@
+from collections import deque
+
 def solution(skill, skill_trees):
     answer = 0
-    skill = list(skill)
-    for s in skill_trees:
-        stack = []
-        for a in s:
-            if a in skill and skill.index(a) >= 1:
-                if not skill[skill.index(a)-1] in stack:
-                    break
-            stack.append(a)
+#     skill = list(skill)
+#     for s in skill_trees:
+#         stack = []
+#         for a in s:
+#             if a in skill and skill.index(a) >= 1:
+#                 if not skill[skill.index(a)-1] in stack:
+#                     break
+#             stack.append(a)
 
-        if len(stack) == len(s):
+#         if len(stack) == len(s):
+#             answer += 1
+    for skills in skill_trees:
+        skill_list = deque(list(skill))
+        
+        for s in skills:
+            if s in skill and s != skill_list.popleft():
+                break
+        else:
             answer += 1
 
     return answer
