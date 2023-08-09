456d7d5d07dae4a95604aa3869d68ec66072c045
Lee Yuri <leeyuri.iosapp@gmail.com>
2023-08-09 19:35:03 +0900

    [level 2] Title: 문자열 압축, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.py
@@ -7,12 +7,18 @@ def solution(s):
         string = ''
         cnt = 1
         for j in range(0, len(s), i):
-            if s[j:j+i] == s[j+i:j+2*i]:
+            comp = s[j:j+i]
+            """
+            오답노트
+            j가 len(s)까지 가는데 왜 10번째줄에서 인덱스 에러가 나지 않을까?
+            파이썬에서는 인덱스 초과 접근 시 에러가 나지만, 슬라이스에서는 알아서 초과되지 않는 범위에서 잘라준다.
+            """
+            if comp == s[j+i:j+2*i]:
                 cnt += 1
             else:
                 if (cnt != 1):
                     string += str(cnt)
-                string += s[j:j+i]
+                string += comp
                 cnt = 1
 
         answer = min(answer, len(string))
