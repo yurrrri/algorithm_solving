33c5a7113ca063a77aa2a491ab83e5b5b58d986d
Lee Yuri <leeyuri.iosapp@gmail.com>
2023-08-22 19:20:08 +0900

    [Gold II] Title: 트리의 지름, Time: 660 ms, Memory: 70904 KB -BaekjoonHub

---
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.py
@@ -0,0 +1,37 @@
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+graph = [[] for _ in range(n+1)]
+first = 0
+
+for _ in range(n):
+  read = list(map(int, input().rstrip().split()))
+  for idx in range(1, len(read)-2, 2):
+    graph[read[0]].append((read[idx], read[idx+1]))
+
+answer = 0
+def bfs(start, n):
+  visited = [-1] * n
+  visited[start] = 0
+  q = deque([start])
+  maxValue = (0, 0) # 정점, 거리
+
+  while q:
+    node = q.popleft()
+
+    for n, d in graph[node]:
+      if visited[n] == -1:
+        visited[n] = visited[node] + d
+        q.append(n)
+        if visited[n] > maxValue[1]:
+          maxValue = (n, visited[n])
+
+  return maxValue
+
+node, dist = bfs(1, n+1)
+node, dist = bfs(node, n+1)
+
+print(dist)
