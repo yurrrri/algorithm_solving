e82e8e17366d0da7fb38da6606b23eecb3392b17
Lee Yuri <leeyuri.iosapp@gmail.com>
2023-08-23 13:43:49 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.08 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.swift
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.swift
@@ -7,18 +7,18 @@ func solution(_ bridge_length:Int, _ weight:Int, _ truck_weights:[Int]) -> Int {
     var bridge_weight = 0 // 다리 위의 무게
     
     // 다리에는 최대 bridge_length 만큼만 트럭이 올라갈 수 있으며, weight 이하의 무게를 견딜 수 있음
-    while !truck_weights.isEmpty || bridge_weight != 0{
+    while !passing.isEmpty {
         //카운트를 하고
         answer += 1
-        //다리를 다 건넌 트럭의 무게를 뺴줌(트럭이 아니라면 0을 뺌
+        //다리를 다 건넌 트럭의 무게를 뺴줌
         bridge_weight -= passing.removeFirst()
         
         //대기중인 트럭이 있다면
         if !truck_weights.isEmpty {
-            if bridge_weight + truck_weights[0] <= weight {
-                passing.append(truck_weights[0])
-                bridge_weight += truck_weights.removeFirst()
-            } else {
+            if bridge_weight + truck_weights.first! <= weight { // 대기중인 트럭 다리 건너게 하기
+                passing.append(truck_weights.first!)
+                bridge_weight += truck_weights.removeFirst()  // 무게 추가
+            } else { // 못보낸다면 0 추가하기
                 passing.append(0)
             }
         } 
