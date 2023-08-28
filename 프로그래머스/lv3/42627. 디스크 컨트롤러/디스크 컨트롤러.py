ef048cdea405048d3a18e2e3e50a494d6e67a23d
Lee Yuri <leeyuri.iosapp@gmail.com>
2023-08-28 22:47:45 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.00 ms, Memory: 9.96 MB -BaekjoonHub

--- 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,22 +1,23 @@
 import heapq
 
 def solution(jobs):
-    answer, now, i = 0, 0, 0
-    start = -1 
+    answer = 0
     heap = []
+    start, now = -1, 0  # 각 작업의 작업 시작 시간, 작업이 끝나면서 현재 시간
+    i = 0 # 작업의 현재 인덱스
     
-    while i < len(jobs):  # 작업이 남아있는 동안 아래 로직을 실행
-        for j in jobs:
-            if start < j[0] <= now: # 이전 작업의 요청 시간보다 크면서 현재 시간보다 작거나 같은 작업 추가
-                heapq.heappush(heap, [j[1], j[0]])
-        
-        if len(heap) > 0: # 처리할 작업이 있는 경우, 작업처리
-            cur = heapq.heappop(heap)
-            start = now
-            now += cur[0] # 작업을 끝낸 현재 시간 --> now + 작업시간
-            answer += now - cur[1] # 작업 요청시간부터 종료시간까지의 시간 계산
-            i += 1 # 작업을 처리했으므로 i+1
-        else: # 처리할 작업이 없는 경우에 시간이 흘러가므로 now + 1
+    while i < len(jobs):
+        for job in jobs:
+            if start < job[0] <= now:  # 만약 처리안한 작업중에 이전 작업 요청시간보다 크면서 현재 시간보다  작거나 같은 작업이 있다면, 힙 작업열에 추가
+                heapq.heappush(heap, (job[1], job[0]))
+                
+        if heap: # 만약 처리할 작업이 있다면
+            time, req = heapq.heappop(heap)  # 차례대로 소요시간, 요청시간
+            start = now  # 작업 시작 시간을 현재 시간으로 갱신
+            now += time   # 현재 시간 갱신
+            answer += now - req # 현재에서 작업 요청시간을 뺀 것을 더하면 요청부터 종료까지의 소요시간을 더하는것임
+            i += 1 # 작업을 완료했으므로 인덱스 증가
+        else:   # 처리할 작업이 없다면, 시간은 흘러가므로 now + 1
             now += 1
                 
     return answer // len(jobs)
