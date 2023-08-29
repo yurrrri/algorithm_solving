a099b53e28d74b33a5b29082cfdac606b4114e0b
Lee Yuri <leeyuri.iosapp@gmail.com>
2023-08-29 22:10:04 +0900

    [level 2] Title: 피로도, Time: 0.05 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.swift
+++ 프로그래머스/lv2/87946. 피로도/피로도.swift
@@ -1,16 +1,16 @@
 import Foundation
 
 func solution(_ k:Int, _ dungeons:[[Int]]) -> Int {
-    
+
     var answer = 0
     var visited = Array(repeating: false, count: dungeons.count)
-    
+
     func dfs(_ count: Int, _ k: Int) {
         if count > dungeons.count {
             return
         }
         answer = max(answer, count)
-        
+
         for i in 0..<dungeons.count {
             if !visited[i] && k >= dungeons[i][0] {
                 visited[i] = true
@@ -19,8 +19,8 @@ func solution(_ k:Int, _ dungeons:[[Int]]) -> Int {
             }
         }
     }
-    
+
     dfs(0, k)
-    
+
     return answer
 }
