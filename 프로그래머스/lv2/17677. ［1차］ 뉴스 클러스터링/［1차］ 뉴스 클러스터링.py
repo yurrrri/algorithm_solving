e51ab6c795381aeeb1dda3a9414caac4d15f3f8e
Lee Yuri <leeyuri.iosapp@gmail.com>
2023-08-29 14:42:43 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.13 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import Counter
 
 def solution(str1, str2):
     answer = 0 # 나중에 65536 곱한 후에 소수점 아래 버린 정수부 출력
@@ -8,22 +8,22 @@ def solution(str1, str2):
     if str1 == str2:
         return 65536
         
-    dic1 = defaultdict(int)
-    dic2 = defaultdict(int)
+    s1 = []
+    s2 = []
     
     len1 = 0
     len2 = 0 
     
     for i in range(len(str1)):
         if str1[i:i+2].isalpha() and len(str1[i:i+2]) == 2 and ' ' not in str1[i:i+2]:
-            dic1[str1[i:i+2]] += 1
+            s1.append(str1[i:i+2])
         
     for j in range(len(str2)):
         if str2[j:j+2].isalpha() and len(str2[j:j+2]) == 2 and ' ' not in str2[j:j+2]:
-            dic2[str2[j:j+2]] += 1
+            s2.append(str2[j:j+2])
             
-    print(dic1)
-    print(dic2)
+    dic1 = Counter(s1)
+    dic2 = Counter(s2)
     
     for key, value in dic1.items():
         if key in dic2:
@@ -35,9 +35,19 @@ def solution(str1, str2):
     for key, value in dic2.items():
         if key not in dic1:
             len2 += dic2[key]
-            
-    # print(len1)
-    # print(len2)
     
     return int(len1/len2 * 65536)
+
+# 다른 풀이
+def solution(str1, str2):
+    # make sets
+    s1 = [str1[i:i+2].lower() for i in range(len(str1)-1) if str1[i:i+2].isalpha()]
+    s2 = [str2[i:i+2].lower() for i in range(len(str2)-1) if str2[i:i+2].isalpha()]
+    if not s1 and not s2:
+        return 65536
+    c1 = Counter(s1)
+    c2 = Counter(s2)
+    answer = int(float(sum((c1&c2).values()))/float(sum((c1|c2).values())) * 65536)
+    
+    return answer
         
