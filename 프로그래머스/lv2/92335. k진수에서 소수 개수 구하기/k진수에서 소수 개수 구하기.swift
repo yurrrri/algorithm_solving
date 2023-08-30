a9343d9d50e606b7031182c85c6e84e1244c8768
Lee Yuri <leeyuri.iosapp@gmail.com>
2023-08-30 17:22:09 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.10 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.swift
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.swift
@@ -1,12 +1,42 @@
 import Foundation
 
+// func radixChange(_ n: Int, _ radix: Int) -> String {
+//     var nums:[String] = []
+//     var n = n
+    
+//     while n > 0 {
+//         var remain = n%radix
+//         n /= radix
+        
+//         nums.append(String(remain))  // 나머지를 차례대로 배열에 넣은 후
+//     }
+    
+//     return Array(nums.reversed()).joined()  // 거꾸로 뒤집어서 합친 문자열 반환
+// }
+
 func solution(_ n:Int, _ k:Int) -> Int {
     var splited = String(n, radix: k).split(separator:"0").filter { $0 != "1" }
+    /* 여기서 0을 기준으로 separate를 하는 이유
+    
+    */
     var arr = splited.map { Int(String($0))! }
     var answer = 0
     
     for e in arr {
         var flag = false
+        // var visited = Array(repeating: true, count: e+1)
+        // for i in 2...Int(sqrt(Double(e))) + 1 {
+        //     if visited[i] {
+        //         var j = 2
+        //         while (i*j) <= e {
+        //             visited[i*j] = false
+        //             j += 1
+        //         }
+        //     }
+        // }
+        // if visited[e] {
+        //     answer += 1
+        // }
         for i in 2...Int(sqrt(Double(e))) + 1 {
             if e%i == 0 && e != i {
                 flag = true
