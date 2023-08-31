d976d58c55ab8740b71386cd96c3899786f4f211
Lee Yuri <leeyuri.iosapp@gmail.com>
2023-08-31 15:51:58 +0900

    [level 2] Title: [3차] n진수 게임, Time: 12.57 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.swift
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.swift
@@ -1,39 +1,37 @@
 import Foundation
 
-func radixChange(_ t: Int, _ n: Int) -> String {
-    var t = t
-    var nums:[String] = []
-    let alpha = ["A", "B", "C", "D", "E", "F"]
-    var remains = 0
-    if t == 0 {
-        return String(t)
-    }
+// func radixChange(_ t: Int, _ n: Int) -> String {
+//     var t = t
+//     var nums:[String] = []
+//     let alpha = ["A", "B", "C", "D", "E", "F"]
+//     var remains = 0
+//     if t == 0 {
+//         return String(t)
+//     }
     
-    while t > 0 {
-        remains = t%n
-        t /= n
+//     while t > 0 {
+//         remains = t%n
+//         t /= n
         
-        if n == 16 && remains >= 10 { // 16진수인 경우에 10~15는 알파벳으로 표시
-            nums.append(alpha[remains-10])
-        } else {
-            nums.append(String(remains))
-        }
-    }
+//         if n == 16 && remains >= 10 { // 16진수인 경우에 10~15는 알파벳으로 표시
+//             nums.append(alpha[remains-10])
+//         } else {
+//             nums.append(String(remains))
+//         }
+//     }
     
-    return Array(nums.reversed()).joined()
-}
+//     return Array(nums.reversed()).joined()
+// }
 
 func solution(_ n:Int, _ t:Int, _ m:Int, _ p:Int) -> String {
     // n: 진법, t: 미리 구할 숫자의 개수, m: 게임 참여 인원, p: 정답이 말해야하는 순서
-    var str = ""
+    var arr:[String] = []
     for i in 0..<t*m {  // 게임 참여 인원 vs 튜브가 말해야하는 숫자 개수 곱함
-        str += String(i, radix:n).uppercased()
+        arr += String(i, radix:n).uppercased().map { String($0) }
     }
-    var arr = str.map { String($0) }
-    // print(arr)
     var result = ""
 
-    for i in stride(from: p-1, to: m*(t-1)+p, by:m) {
+    for i in stride(from: p-1, to: t*m, by:m) {
         result += arr[i]
     }
     
