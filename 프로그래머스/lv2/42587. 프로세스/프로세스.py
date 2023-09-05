58b6c6b1e6cb188e21c569a4047bbcb4a9367604
Lee Yuri <leeyuri.iosapp@gmail.com>
2023-09-06 01:10:13 +0900

    [level 2] Title: 프로세스, Time: 0.12 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프로세스/프로세스.py
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.py
@@ -1,22 +1,11 @@
-from collections import deque
-
 def solution(priorities, location):
-    priority = [(idx, e) for idx, e in enumerate(priorities)]
-    q = deque(priority)
+    q = [(i, e) for i, e in enumerate(priorities)]
     answer = 0
-    
     while True:
-        prior = q.popleft() # 1. 프로세스 꺼내기
-        if not q:
-            answer += 1
-            break
-        
-        maxValue = max(q, key=lambda x:x[1])[1] # 우선순위 최대값
-        if prior[1] < maxValue: # 큐에 대기중인 프로세스 중 우선순위가 더 높은 프로세스 존재
-            q.append(prior)
+        i, e = q.pop(0)
+        if any(e < a[1] for a in q):
+            q.append((i, e))
         else:
             answer += 1
-            if prior[0] == location:
-                break
-                
-    return answer
+            if i == location:
+                return answer
