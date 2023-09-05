0fba950b7dd1b018730a6b9214427eda4b53a1f6
Lee Yuri <leeyuri.iosapp@gmail.com>
2023-09-05 23:49:37 +0900

    [Gold V] Title: 숫자고르기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.py
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.py
@@ -1,31 +1,37 @@
+import sys
+
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+def dfs(node):
+  global result
+  
+  cycle.append(node)
+  visited[node] = True
+  next = graph[node]
+
+  if visited[next]:
+    if next in cycle:
+      result += cycle[cycle.index(next):]
+      return
+  else:
+    dfs(next)
+  
 n = int(input())
-graph = [[] for _ in range(n+1)]
+graph = [0]
 
 for a in range(1, n+1):
-    b = int(input())
-    graph[b].append(a)
+  b = int(input())
+  graph.append(b)
 
 visited = [False] * (n+1) # 노드 방문 여부 확인
 
 result = [] # 결과 집합
-# route 는 집합
-def dfs(node, route):
-    global result
-  
-    route.append(node)
-    visited[node] = True
-    for i in graph[node]:
-        if i not in route:
-            dfs(i, route.copy())
-        else:# 이미 node가 route에 포함되어있으므로 사이클이 생기는 경우 추가
-            result += list(route)
-            return
-
 for i in range(1, n+1):
-    if not visited[i]:
-        dfs(i, [])
+  if not visited[i]:
+    cycle = []
+    dfs(i)
 
-result.sort()
 print(len(result))
-for i in result:
-    print(i)
+for i in sorted(result):
+  print(i)
