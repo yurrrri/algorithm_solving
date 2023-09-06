e267847c69119af706a652839b7944e4ab7e21cf
Lee Yuri <leeyuri.iosapp@gmail.com>
2023-09-06 21:59:16 +0900

    [level 2] Title: 주식가격, Time: 29.97 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.py
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -1,19 +1,15 @@
 def solution(prices):
     answer = [0] * len(prices)
-    stack = []
+    stack = []   # 인덱스를 쌓아둘 스택
+    
     for i in range(len(prices)):
-        """
-        2. 인덱스를 넣기 전에, stack의 마지막 인덱스가 현재보다 크다면 떨어졌다는 의미이므로 꺼내고 answer에 저장
-        """
-        while stack and prices[stack[-1]] > prices[i]: 
-            past = stack.pop()
-            answer[past] = i-past
-        stack.append(i)  # 1. 차례대로 가격의 각 인덱스를 넣어줌
+        while stack and prices[stack[-1]] > prices[i]:
+            poped = stack.pop()
+            answer[poped] = i - poped
+            
+        stack.append(i)   # 1. 차례대로 인덱스를 추가함
         
     for i in stack:
         answer[i] = len(prices) - 1 - i
-    """
-    3. 끝까지 상승세를 지속하다가 끝난 경우를 모두 계산함
-    """
         
     return answer
