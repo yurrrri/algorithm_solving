a49f9cd5af50b96331825fa0904719fbe04bc904
Lee Yuri <leeyuri.iosapp@gmail.com>
2023-09-06 22:26:54 +0900

    [level 2] Title: 소수 찾기, Time: 0.41 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/README.md
+++ 프로그래머스/lv2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=python3) 
 
 ### 성능 요약
 
-메모리: 16.3 MB, 시간: 0.77 ms
+메모리: 10.4 MB, 시간: 0.41 ms
 
 ### 구분
 
---
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
@@ -0,0 +1,31 @@
+from itertools import permutations
+from math import sqrt
+
+def isPrime(n):
+    if n <= 1:
+        return False
+    elif n <= 3:
+        return True
+    else:
+        for i in range(2, int(sqrt(n))+1):
+            if n%i == 0:
+                return False
+        return True
+
+
+def solution(numbers):
+    answer = 0
+    numbers = list(numbers)
+    
+    combi = set()
+    for i in range(1, len(numbers)+1):
+        arr = list(permutations(numbers, i))
+        for j in arr:
+            combi.add(''.join(j))
+    
+    answer = set()
+    for c in combi:
+        if isPrime(int(c)):
+            answer.add(int(c))
+    
+    return len(answer)
