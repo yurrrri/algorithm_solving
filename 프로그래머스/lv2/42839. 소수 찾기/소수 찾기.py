ed10142eae6225bf7874a9462fea06097580fdb0
Lee Yuri <leeyuri.iosapp@gmail.com>
2023-09-06 22:34:48 +0900

    [level 2] Title: 소수 찾기, Time: 0.25 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
@@ -1,5 +1,4 @@
 from itertools import permutations
-from math import sqrt
 
 def isPrime(n):
     if n <= 1:
@@ -7,7 +6,7 @@ def isPrime(n):
     elif n <= 3:
         return True
     else:
-        for i in range(2, int(sqrt(n))+1):
+        for i in range(2, int(n**0.5)+1):
             if n%i == 0:
                 return False
         return True
@@ -16,16 +15,16 @@ def isPrime(n):
 def solution(numbers):
     answer = 0
     numbers = list(numbers)
-    
+
     combi = set()
     for i in range(1, len(numbers)+1):
         arr = list(permutations(numbers, i))
         for j in arr:
             combi.add(''.join(j))
-    
+
     answer = set()
     for c in combi:
         if isPrime(int(c)):
             answer.add(int(c))
-    
+
     return len(answer)
