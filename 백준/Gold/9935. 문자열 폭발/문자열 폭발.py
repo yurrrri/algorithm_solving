160dcaff921f9ae86f8a9a10e68383c61c87e62b
Lee Yuri <leeyuri.iosapp@gmail.com>
2023-09-07 15:39:31 +0900

    [Gold IV] Title: 문자열 폭발, Time: 660 ms, Memory: 42436 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -6,11 +6,11 @@ boom = list(input().rstrip())
 
 stack = []
 
-for i in str:   # 1_000_000보다 작거나 같음
+for i in str:   # 1_000_000보다 작거나 같음 --> is in과 replace를 쓰면 시간초과
   stack.append(i)
 
   while len(stack) >= len(boom) and stack[-len(boom):] == boom:
-    stack[-len(boom):] = []
+    del stack[-len(boom):]
   
 if not stack:
   print("FRULA")
