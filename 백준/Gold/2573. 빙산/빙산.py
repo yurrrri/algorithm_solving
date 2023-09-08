998aec1f0684e1e177daf47668967bc4295a2587
Lee Yuri <leeyuri.iosapp@gmail.com>
2023-09-08 21:07:52 +0900

    [Gold IV] Title: 빙산, Time: 724 ms, Memory: 207052 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,57 +1,50 @@
-import sys
 from collections import deque
 
-input = sys.stdin.readline
-n, m = map(int, input().rstrip().split())
-board = []
+n, m = map(int, input().split())
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
+board = [list(map(int, input().split())) for _ in range(n)]
 
-for _ in range(n):
-  board.append(list(map(int, input().rstrip().split())))
+def bfs(i, j):
+  visited[i][j] = True
+  q = deque([(i, j)])
 
-def bfs(x, y):
-  q = deque([(x, y)])
-  visited[x][y] = True
-  
   while q:
     x, y = q.popleft()
-    
+
     for i in range(4):
       nx = x + dx[i]
       ny = y + dy[i]
-  
+
       if 0<=nx<n and 0<=ny<m:
-        if board[nx][ny] != 0 and not visited[nx][ny]:
+        if board[nx][ny] == 0:
+          count[x][y] += 1
+        elif board[nx][ny] != 0 and not visited[nx][ny]:
           q.append((nx, ny))
           visited[nx][ny] = True
-        elif board[nx][ny] == 0:
-          count[x][y] += 1
-
-  return 1  # 구역의 수
-
-time = 0
+  for a in range(n):
+    for b in range(m):
+      board[a][b] -= count[a][b]
+      if board[a][b] < 0:
+        board[a][b] = 0
+        
+answer = 0
 while True:
-  visited = [[False] * m for _ in range(n)] #탐색 할때마다 visited 초기화
-  count = [[0] * m for _ in range(n)] #탐색 할때마다 visited 초기화
-  ans = 0
+  visited = [[False] * m for _ in range(n)]
+  count = [[0] * m for _ in range(n)]
+  area_count = 0
   
   for i in range(n):
     for j in range(m):
       if board[i][j] != 0 and not visited[i][j]:
-        ans += bfs(i, j)
+        bfs(i, j)
+        area_count += 1
 
-  if ans == 0:
+  if area_count == 0:
     print(0)
     break
-  elif ans >= 2:
-    print(time)
+  elif area_count >= 2:
+    print(answer)
     break
 
-  for i in range(n):
-    for j in range(m):
-      board[i][j] -= count[i][j]
-      if board[i][j] < 0:
-        board[i][j] = 0
-
-  time += 1
+  answer += 1
