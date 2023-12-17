b21abb9167e6e35b37f584d2e046bd1f68819119
Lee Yuri <leeyuri.iosapp@gmail.com>
2023-12-17 18:04:53 +0900

    [level 2] Title: 행렬의 곱셈, Time: 8.30 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -3,8 +3,10 @@ def solution(arr1, arr2):
     r2, c2 = len(arr2), len(arr2[0])
     
     answer = [[0] * c2 for _ in range(r1)]
+    
     for i in range(r1):
-        for j in range(c2):
-            for k in range(c1):
-                answer[i][j] += arr1[i][k] * arr2[k][j]
+        for j in range(c1):
+            for k in range(c2):
+                answer[i][k] += arr1[i][j] * arr2[j][k]
+    
     return answer
