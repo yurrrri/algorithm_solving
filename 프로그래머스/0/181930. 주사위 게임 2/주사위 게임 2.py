05ef0062f7e75be7c62c24d014cc4abf048f37c8
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-02-21 00:25:38 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -0,0 +1,7 @@
+def solution(a, b, c):
+    if len(set([a, b, c])) == 3:
+        return a+b+c
+    elif len(set([a, b, c])) == 2:
+        return (a+b+c) * (a**2 + b**2 + c**2)
+    else:
+        return (a+b+c) * (a**2 + b**2 + c**2) * (a**3 + b**3 + c**3)
