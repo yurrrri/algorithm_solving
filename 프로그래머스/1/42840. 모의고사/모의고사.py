0f3d314dab152571ecd2de6922f752a944bcc8b7
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-02-22 00:13:22 +0900

    [level 1] Title: 모의고사, Time: 2.05 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -2,17 +2,17 @@ def solution(answers):
     first_student = [1, 2, 3, 4, 5] * (len(answers)//5 + 1)
     second_student = [2, 1, 2, 3, 2, 4, 2, 5] * (len(answers)//8 + 1)
     third_student = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] * (len(answers)//10 + 1)
-    student_score_list = [0, 0, 0]
+    student_score_list = [0] * 3
+    
     answer = []
     
-    for i in range(len(answers)):
-        if first_student[i] == answers[i]:
+    for i, score in enumerate(answers):
+        if first_student[i] == score:
             student_score_list[0] += 1
-        if second_student[i] == answers[i]:
+        if second_student[i] == score:
             student_score_list[1] += 1
-        if third_student[i] == answers[i]:
+        if third_student[i] == score:
             student_score_list[2] += 1
             
-    max_num = max(student_score_list)
-    answer = [i+1 for i in range(3) if student_score_list[i] == max_num]
+    answer = [i+1 for i, total in enumerate(student_score_list) if total == max(student_score_list)]
     return answer
