f4cdb6d6a3af88461deeb7c186b3af257859073f
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-02-21 19:44:54 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.08 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
@@ -1,7 +1,4 @@
 def solution(arr, queries):
-    answer = arr
-    for q in queries:
-        a = answer[q[1]]
-        answer[q[1]] = answer[q[0]]
-        answer[q[0]] = a
-    return answer
+    for a, b in queries:
+        arr[a], arr[b] = arr[b], arr[a]
+    return arr
