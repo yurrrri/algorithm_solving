5cd76f746568564174ce90b8314ac8bf7d5bc833
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-02-21 19:25:00 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
@@ -1,7 +1,6 @@
 def solution(a, d, included):
     answer = 0
-    num_list = [a + d*i for i in range(len(included))]
     for idx in range(len(included)):
         if included[idx]:
-            answer += num_list[idx]
+            answer += a + d*idx
     return answer
