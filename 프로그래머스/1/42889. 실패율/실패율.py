44783a1d8f4a0c71a4be56e6a3d8ef7c159301fc
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-02-28 00:53:52 +0900

    [level 1] Title: 실패율, Time: 19.10 ms, Memory: 18.3 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,10 +1,10 @@
 def solution(N, stages):
     bunmo = len(stages)  # 처음 분모 stages의 길이로 시작
     counts = [0] * (N+2) # 분자 저장할 리스트
-    dic = {} 
+    dic = {}  # 키 - 쌍: 딕셔너리 활용
     
     for stage in stages:
-        counts[stage] += 1
+        counts[stage] += 1     # 먼저 반복문을 통해 값을 미리 저장해두기 (시간복잡도 줄이기)
         
     for i in range(1, N+1):
         if counts[i] == 0:
@@ -12,7 +12,5 @@ def solution(N, stages):
         else:
             dic[i] = counts[i] / bunmo
             bunmo -= counts[i]
-            
-    print(dic)
-            
-    return sorted(dic, key=lambda x:dic[x], reverse=True)
+                        
+    return sorted(dic, key=lambda x:dic[x], reverse=True)  #딕셔너리 정렬 -> 키값만 반환한 리스트
