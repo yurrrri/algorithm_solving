02b676141819d5dd011d915c53b52ccfc6bb8693
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-02-28 00:51:49 +0900

    [level 1] Title: 실패율, Time: 19.56 ms, Memory: 15 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,18 +1,18 @@
-from collections import Counter
-
 def solution(N, stages):
-    counter = dict(Counter(stages))
-    count = len(stages)
+    bunmo = len(stages)  # 처음 분모 stages의 길이로 시작
+    counts = [0] * (N+2) # 분자 저장할 리스트
+    dic = {} 
     
-    dic = {}
-    
+    for stage in stages:
+        counts[stage] += 1
+        
     for i in range(1, N+1):
-        if i not in counter.keys():
+        if counts[i] == 0:
             dic[i] = 0
-            continue
-        dic[i] = counter[i] / count
-        count -= counter[i]
-        
-    answer = sorted(dic, key=lambda x:dic[x], reverse=True)
-        
-    return answer
+        else:
+            dic[i] = counts[i] / bunmo
+            bunmo -= counts[i]
+            
+    print(dic)
+            
+    return sorted(dic, key=lambda x:dic[x], reverse=True)
