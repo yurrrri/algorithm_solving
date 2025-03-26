9b42b269ea5334ba018b6c8eae4d58e453337c04
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-03-27 03:57:53 +0900

    [level 2] Title: 기능개발, Time: 0.49 ms, Memory: 9.33 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -7,12 +7,12 @@ def solution(progresses, speeds):
     answer = []
     count = 0
     
-    while len(queue) > 0:
-        while queue[0] < 100:
+    while len(queue) > 0:  # 모든 작업이 끝날떄까지 반복
+        while queue[0] < 100:   # 앞 작업 진척률이 100 되기 전까지 모든 작업상황들 진척상황 업데이트
             for i in range(len(speed_queue)):
                 queue[i] += speed_queue[i]
                 
-        while len(queue) > 0 and queue[0] >= 100:
+        while len(queue) > 0 and queue[0] >= 100:  # 앞에 있는 작업이 100 넘는것들은 모두 배포
             queue.popleft()
             speed_queue.popleft()
             count += 1
