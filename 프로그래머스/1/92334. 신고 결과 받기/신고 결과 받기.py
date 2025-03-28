461116ed2abd1a2661df4d54c211c51457c21a6b
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-03-29 02:49:36 +0900

    [level 1] Title: 신고 결과 받기, Time: 223.94 ms, Memory: 43.3 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,18 +1,18 @@
 def solution(id_list, report, k):
     result = []
-    count_dic = {}   # 신고당한 횟수를 담을 딕셔너리
-    report_dic = {}  # 각 유저마다 신고한 유저를 담을 딕셔너리
-    for id in id_list:  # 초기화
-        count_dic[id] = 0
-        report_dic[id] = []
-    
+    count_dic = {id: 0 for id in id_list}   # 신고당한 횟수를 담을 딕셔너리
+    report_dic = {id: set() for id in id_list}  # 각 유저마다 신고한 유저를 담을 딕셔너리
+
     for r in report:
         reporter, reported = r.split()
         if reported not in report_dic[reporter]:
-            report_dic[reporter].append(reported)
+            report_dic[reporter].add(reported)
+            
+    for reporters in report_dic.values():
+        for reported in reporters:
             count_dic[reported] += 1
 
-    for key, v in report_dic.items():  #(10^3)
+    for key, v in report_dic.items():
         if len(v) == 0:
             result.append(0)
             continue
@@ -21,5 +21,5 @@ def solution(id_list, report, k):
             if count_dic[reported] >= k:
                 count += 1
         result.append(count)
-    
+
     return result
