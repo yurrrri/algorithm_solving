91b657a5811b6dc51b243a0326d85118c3ba1841
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-03-29 03:43:21 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 2.87 ms, Memory: 9.68 MB -BaekjoonHub

---
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -0,0 +1,26 @@
+from itertools import combinations
+from collections import Counter
+
+
+def solution(orders, course):
+    answer = []
+    combi = []
+    
+    for order in orders:
+        order_to_list = sorted(list(order))
+        for c in course:
+            combi.extend(combinations(order_to_list, c))
+            
+    counter = Counter(combi)
+    
+    for c in course:
+        max_num = 0
+        for k, v in counter.items():
+            if len(k) == c and v >= 2:
+                max_num = max(v, max_num)
+                
+        for k, v in counter.items():
+            if len(k) == c and v == max_num:
+                answer.append("".join(k))
+        
+    return sorted(answer)
