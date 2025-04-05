7bbd8f762621fde5b9eee327c2e176ddc7959446
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-04-05 23:59:38 +0900

    [level 2] Title: 배달, Time: 1.59 ms, Memory: 9.63 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -6,25 +6,25 @@ def solution(N, road, K):
     graph = [[] for _ in range(N+1)]
     q = []
     
-    for r in road:
-        graph[r[0]].append((r[1], r[2]))
-        graph[r[1]].append((r[0], r[2]))
+    for a, b, c in road:
+        graph[a].append((b, c))   # 양방향으로 연결
+        graph[b].append((a, c))
     
-    distance[1] = 0
-    heapq.heappush(q, (0, 1))
+    distance[1] = 0   # 1번 마을에서 시작
+    heapq.heappush(q, (0, 1))    # 1번 마을이 시작점이므로, 0으로 초기화
     
     while q:
         dist, now = heapq.heappop(q)
         
-        if distance[now] < dist:
+        if distance[now] < dist:   # 이미 계산한 노드라면 계산을 건너뜀
             continue
             
-        for i in graph[now]:
-            cost = dist + i[1]
+        for next_node, next_cost in graph[now]:       # 인접한 노드들 간의 모든 최단거리 구하기
+            cost = dist + next_cost
             
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
+            if cost < distance[next_node]:
+                distance[next_node] = cost
+                heapq.heappush(q, (cost, next_node))
                 
     answer = 0
 
