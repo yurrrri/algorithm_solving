5f47552e2347a168f2e5b4c413a668bbc818da87
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-04-08 02:10:44 +0900

    [Silver I] Title: 스티커, Time: 616 ms, Memory: 41436 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -5,7 +5,6 @@ t = int(input())
 
 for _ in range(t):
   dp = []
-  answer = 0
   num = int(input())
   for _ in range(2):
     dp.append(list(map(int, input().split())))
@@ -19,7 +18,7 @@ for _ in range(t):
     dp[1][1] += dp[0][0]
 
     for i in range(2, num):
-      dp[0][i] = dp[0][i] + max(dp[1][i-1], dp[1][i-2], dp[0][i-2])
-      dp[1][i] = dp[1][i] + max(dp[0][i-1], dp[0][i-2], dp[1][i-2])
+      dp[0][i] = dp[0][i] + max(dp[1][i-1], dp[1][i-2])
+      dp[1][i] = dp[1][i] + max(dp[0][i-1], dp[0][i-2])
 
     print(max(dp[0][num-1], dp[1][num-1]))
