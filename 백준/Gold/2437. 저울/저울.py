62474a838486162ba41bfea19343222b8b0e760a
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-04-10 17:35:39 +0900

    [Gold II] Title: 저울, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2437. 저울/저울.py
+++ 백준/Gold/2437. 저울/저울.py
@@ -2,10 +2,11 @@ n = int(input())
 arr = list(map(int, input().split()))
 arr.sort()
 
-target = 1
-for num in arr:
-  if target < num:
-    break
-  target += num
+answer = 0  # 지금까지 측정 가능한 최대 무게 (초기: 0)
 
-print(target)
+for w in arr:
+    if w > answer + 1:
+      break
+    answer += w  # 기존 구간 확장
+
+print(answer + 1)  # 끝까지 연결되었으면 다음 숫자부터 측정 불가
