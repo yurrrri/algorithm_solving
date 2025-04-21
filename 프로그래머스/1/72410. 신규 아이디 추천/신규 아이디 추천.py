8c715e74012c06abd5f06e7d51cda1b678e0e649
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-04-21 17:37:03 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.37 ms, Memory: 9.12 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -0,0 +1,27 @@
+def solution(new_id):
+    # 1단계
+    answer = new_id.lower()
+    # 2단계
+    temp = ""
+    for c in answer:
+        if c.islower() or c.isdigit() or c in ["-", "_", "."]:
+            temp += c
+    answer = temp
+    # 3단계
+    while ".." in answer:
+        answer = answer.replace("..", ".")
+    # 4단계
+    answer = answer.strip(".")
+    # 5단계
+    if len(answer) == 0:
+        answer = "a"
+    # 6단계
+    if len(answer) >= 16:
+        answer = answer[:15]
+    answer = answer.rstrip(".")
+    # 7단계
+    if len(answer) <= 2:
+        last = answer[-1]
+        while len(answer) < 3:
+            answer += last
+    return answer
