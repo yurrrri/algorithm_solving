3547aed2f7ef75555b63d6350d841e2736113544
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-04-23 21:18:27 +0900

    [Silver I] Title: 안전 영역, Time: 1064 ms, Memory: 34920 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,42 +1,40 @@
 import sys
-from collections import deque
-
-sys.setrecursionlimit(10**8)
-
 input = sys.stdin.readline
-n = int(input())   # 행, 열
-graph = []
+sys.setrecursionlimit(10**6)
 
+n = int(input())
+visited = [[False] * n for _ in range(n)]
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-maxHeight = 0
+graph = []
+maxHeight = -1
 
-for i in range(n):
+for _ in range(n):
   arr = list(map(int, input().split()))
-  maxHeight = max(max(arr), maxHeight)  # 높이의 최대값을 구해 0부터 최대값까지 각 경우의 물에 잠기지 않는 지역 개수를 구하기 위함
+  maxHeight = max(maxHeight, max(arr))
   graph.append(arr)
 
-def dfs(x, y, height):
+def dfs(x, y):
   visited[x][y] = True
-  
+
   for i in range(4):
     nx = x + dx[i]
     ny = y + dy[i]
 
     if 0<=nx<n and 0<=ny<n and not visited[nx][ny] and graph[nx][ny] > height:
-      dfs(nx, ny, height)
+      dfs(nx, ny)
 
 answer = 0
+for height in range(maxHeight+1):
+  visited = [[False] * n for _ in range(n)]
+  area_count = 0
 
-for i in range(0, maxHeight+1):
-  visited = [[False] * (n+1) for _ in range(n+1)]
-  count = 0
-  
-  for j in range(n):
-    for k in range(n):
-      if graph[j][k] > i and not visited[j][k]:
-        count += 1
-        dfs(j, k, i)
-  answer = max(answer, count)
+  for x in range(n):
+    for y in range(n):
+      if graph[x][y] > height and not visited[x][y]:
+        dfs(x, y)
+        area_count += 1
+
+  answer = max(answer, area_count)
 
 print(answer)
