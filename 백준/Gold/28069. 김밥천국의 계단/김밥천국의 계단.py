1892ab5600deeb0d651848efcb7acaaf50c01807
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-04-24 15:34:07 +0900

    [Gold V] Title: 김밥천국의 계단, Time: 856 ms, Memory: 40224 KB -BaekjoonHub

---
+++ 백준/Gold/28069. 김밥천국의 계단/김밥천국의 계단.py
@@ -0,0 +1,15 @@
+import sys
+sys.setrecursionlimit(10**8)
+
+n, k = map(int, input().split())
+INF = int(1e9)
+dp = [INF] * (n+1)
+dp[0] = 0
+
+for i in range(n):
+  if i + 1 <= n:
+    dp[i+1] = min(dp[i+1], dp[i] + 1)
+  if i + i/2 <= n:
+    dp[int(i + i/2)] = min(dp[int(i + i/2)], dp[i] + 1)
+
+print("minigimbob" if dp[n] <= k else "water")
