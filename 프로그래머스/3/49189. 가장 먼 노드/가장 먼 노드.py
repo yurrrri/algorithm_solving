2f7b69a2a71a182640aaefda635714482a25065d
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-04-24 10:06:45 +0900

    [level 3] Title: 가장 먼 노드, Time: 28.05 ms, Memory: 19.7 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -9,13 +9,13 @@ def solution(n, edge):
         graph[b].append(a)
         
     q = deque([1])
-    distance[1] = 1
+    distance[1] = 1    # 방문처리
     while q:
         a = q.popleft()
         
         for node in graph[a]:
             if not distance[node]:
-                distance[node] = distance[a] + 1
+                distance[node] = distance[a] + 1   # 인접한 노드가 1과 연결된 간선의 갯수에 +1을 함
                 q.append(node)
     
     max_vertex = max(distance)
