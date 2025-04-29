5596fafa87d685d26243b515de080b40e29e6db7
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-04-29 22:05:30 +0900

    [level 2] Title: 타겟 넘버, Time: 535.99 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -2,18 +2,18 @@ answer = 0
 
 def solution(numbers, target):
     
-    def dfs(depth, count, index):     
+    def dfs(depth, sum, index):     
         global answer
         
-        if depth == len(numbers) and count == target:
+        if depth == len(numbers) and sum == target:
             answer += 1
             return
         
         if index >= len(numbers):
             return
             
-        dfs(depth+1, count + numbers[index], index+1)
-        dfs(depth+1, count - numbers[index], index+1)
+        dfs(depth+1, sum + numbers[index], index+1)
+        dfs(depth+1, sum - numbers[index], index+1)
         
     dfs(0, 0, 0)
     
