a9167acc7b44dd1e14285aa1a51d6093444ce66d
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-05-01 00:06:39 +0900

    [Gold IV] Title: 스도쿠, Time: 4888 ms, Memory: 154940 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.py
+++ 백준/Gold/2239. 스도쿠/스도쿠.py
@@ -1,23 +1,32 @@
+import sys
+input = sys.stdin.readline
+
 board = []
 
 for _ in range(9):
-  board.append(list(map(int, str(input()))))
+  board.append(list(map(int, str(input().rstrip()))))
 
-def is_in_col(col, num):
-  return num in (board[i][col] for i in range(9))
+def valid_in_col(col, num):
+  for i in range(9):
+    if board[i][col] == num:
+      return False
+  return True
 
-def is_in_row(row, num):
-  return num in board[row]
+def valid_in_row(row, num):
+  for i in range(9):
+    if board[row][i] == num:
+      return False
+  return True
 
-def is_in_box(row, col, num):
+def valid_in_box(row, col, num):
   area_row = (row // 3) * 3
   area_col = (col // 3) * 3
   for i in range(area_row, area_row + 3):
     for j in range(area_col, area_col + 3):
       if board[i][j] == num:
-        return True
+        return False
 
-  return False
+  return True
 
 def find_zero():
   for i in range(9):
@@ -35,12 +44,12 @@ def backtracking():
         print(board[i][j], end = "")
       print()
 
-    exit(0)
+    sys.exit(0)
 
   for i in range(1, 10):
-    if not (is_in_col(y, i) or is_in_row(x, i) or is_in_box(x, y, i)):
+    if valid_in_col(y, i) and valid_in_row(x, i) and valid_in_box(x, y, i):
       board[x][y] = i
       backtracking()
       board[x][y] = 0
-    
+
 backtracking()
