d480a5f188bef801452bdbda1b36389c6839c678
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-05-07 23:02:08 +0900

    [level 3] Title: 베스트앨범, Time: 0.37 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.swift
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.swift
@@ -6,21 +6,23 @@ func solution(_ genres:[String], _ plays:[Int]) -> [Int] {
     var answer:[Int] = []
     
     for (index, genre) in genres.enumerated() {
-        playDic[genre, default:[]].append((index, plays[index]))
-        totalDic[genre, default:0] += plays[index]
+        playDic[genre, default:[]].append((index, plays[index]))  // 장르별 (고유번호, 재생수)
+        totalDic[genre, default:0] += plays[index]   // 장르별 총 재생수
     }
     
-    let sortedTotalDic = totalDic.sorted(by: { $0.1 > $1.1 })
+    let sortedTotalDic = totalDic.sorted(by: { $0.1 > $1.1 })   // 가장 많이 재생된 장르 순서대로 정렬
     
     for (k, v) in sortedTotalDic {
-        let sortedPlayDic = playDic[k]!.sorted(by: { $0.1 > $1.1 })
-        if sortedPlayDic.count >= 2 {
-            answer.append(sortedPlayDic[0].0)
-            answer.append(sortedPlayDic[1].0)
-        } else {
-            answer.append(sortedPlayDic[0].0)
-        }
+        let sortedPlayDic = playDic[k]!.sorted {
+            if $0.1 == $1.1 {
+                return $0.0 < $1.0
+            } else {
+                return $0.1 > $1.1
+            }
+        }.prefix(2)
+        answer.append(contentsOf: sortedPlayDic.map { $0.0 })
     }
+    
     
     return answer
 }
