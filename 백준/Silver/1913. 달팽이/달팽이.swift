2ac19d24f3ae41c48d296d064fa4e7fa62f78a61
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-05-07 18:37:18 +0900

    [Silver III] Title: 달팽이, Time: 256 ms, Memory: 87380 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.swift
+++ 백준/Silver/1913. 달팽이/달팽이.swift
@@ -1,57 +1,44 @@
+import Foundation
+
 let n = Int(readLine()!)!
-let target = Int(readLine()!)!
+let m = Int(readLine()!)!
 
-var array = Array(repeating: Array(repeating: 0, count: n), count: n)
-var start_row = 0, end_row = n - 1
-var start_col = 0, end_col = n - 1
+var arr = Array(repeating: Array(repeating: 0, count: n), count: n)
+
+// 하우상좌
+let dx = [1, 0, -1, 0]
+let dy = [0, 1, 0, -1]
+
+var dir = 0
 var num = n * n
+var x = 0, y = 0
 
-while start_row <= end_row && start_col <= end_col {
-    // 왼쪽 열 (위 → 아래)
-    for i in start_row...end_row {
-        array[i][start_col] = num
-        num -= 1
-    }
-    start_col += 1
+var targetX = 0
+var targetY = 0
 
-    // 아래쪽 행 (왼 → 오른쪽)
-    if start_col <= end_col {
-        for i in start_col...end_col {
-            array[end_row][i] = num
-            num -= 1
-        }
-        end_row -= 1
+while num > 0 {
+    arr[x][y] = num
+    if num == m {
+        targetX = x + 1
+        targetY = y + 1
     }
 
-    // 오른쪽 열 (아래 → 위)
-    if start_row <= end_row && start_col <= end_col {
-        for i in stride(from: end_row, through: start_row, by: -1) {
-            array[i][end_col] = num
-            num -= 1
-        }
-        end_col -= 1
+    var nx = x + dx[dir]
+    var ny = y + dy[dir]
+
+    if !(0 <= nx && nx < n && 0 <= ny && ny < n && arr[nx][ny] == 0) {
+        dir = (dir + 1) % 4
+        nx = x + dx[dir]
+        ny = y + dy[dir]
     }
 
-    // 위쪽 행 (오른쪽 → 왼쪽)
-    if start_row <= end_row && start_col <= end_col {
-        for i in stride(from: end_col, through: start_col, by: -1) {
-            array[start_row][i] = num
-            num -= 1
-        }
-        start_row += 1
-    }
+    x = nx
+    y = ny
+    num -= 1
 }
 
-// 출력 + 좌표 찾기
-var result_row = 0, result_col = 0
-for i in 0..<n {
-    for j in 0..<n {
-        print(array[i][j], terminator: " ")
-        if array[i][j] == target {
-            result_row = i + 1
-            result_col = j + 1
-        }
-    }
-    print()
+// 출력
+for row in arr {
+    print(row.map { String($0) }.joined(separator: " "))
 }
-print("\(result_row) \(result_col)")
+print("\(targetX) \(targetY)")
