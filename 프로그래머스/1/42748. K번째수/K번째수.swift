e914e25d37b55ee0dd69cbdc49ee6a5956bac00b
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-05-09 01:48:46 +0900

    [level 1] Title: K번째수, Time: 0.17 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.swift
+++ 프로그래머스/1/42748. K번째수/K번째수.swift
@@ -1,10 +1,7 @@
 import Foundation
 
 func solution(_ array:[Int], _ commands:[[Int]]) -> [Int] {
-    var answer:[Int] = []
-    for command in commands {
-        let (i, j, k) = (command[0], command[1], command[2])
-        answer.append(array[i-1...j-1].sorted()[k-1])
+    return commands.map {
+        array[$0[0]-1...$0[1]-1].sorted()[$0[2]-1]
     }
-    return answer
 }
