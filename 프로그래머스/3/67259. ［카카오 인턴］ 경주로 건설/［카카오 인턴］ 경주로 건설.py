e4931d2105575e8f7bb7567fb5a372b6b0103c22
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-05-08 19:59:38 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 44.41 ms, Memory: 9.43 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -1,45 +1,3 @@
-# 다익스트라
-# import heapq
-
-# def solution(board):
-#     n = len(board)
-#     INF = int(1e9)
-#     # 방향: 차례대로 상 좌 하 우
-#     dx = [-1, 0, 1, 0]
-#     dy = [0, -1, 0, 1]
-    
-#     # 3차원 거리 배열: [x][y][dir]
-#     distance = [[[INF] * 4 for _ in range(n)] for _ in range(n)]
-    
-#     # 초기 위치 (0, 0) 에 대해 모든 방향 비용 0으로 초기화
-#     for i in range(4):
-#         distance[0][0][i] = 0
-    
-#     heap = []
-#     heapq.heappush(heap, (0, -1, 0, 0))  # 비용, 이전방향, x, y
-
-#     def calculate_cost(direction, prev_direction, cost):
-#         if prev_direction == -1 or (prev_direction - direction) %2 == 0:
-#             return cost + 100
-#         else:
-#             return cost + 600
-
-#     while heap:
-#         cost, direction, x, y = heapq.heappop(heap)
-
-#         for i in range(4):
-#             nx = x + dx[i]
-#             ny = y + dy[i]
-
-#             if 0 <= nx < n and 0 <= ny < n and board[nx][ny] == 0:
-#                 next_cost = calculate_cost(i, direction, cost)
-#                 if distance[nx][ny][i] > next_cost:
-#                     distance[nx][ny][i] = next_cost
-#                     heapq.heappush(heap, (next_cost, i, nx, ny))
-
-#     return min(distance[n - 1][n - 1])  # 도착점까지의 최소 비용
-
-# BFS
 from collections import deque
 
 def solution(board):
@@ -53,14 +11,14 @@ def solution(board):
     # 3차원 거리 배열: [x][y][dir]
     distance = [[[INF] * 4 for _ in range(n)] for _ in range(n)]
 
-    # 초기 위치에서 네 방향 모두 시작 가능
+    # 초기 위치에서 네 방향 모두 시작 가능하므로 모든 후보지에 대한 distance 배열 초기화 및 큐에 추가한다.
     q = deque([])
     for i in range(4):
         distance[0][0][i] = 0
         q.append((0, 0, i, 0))  # x, y, 방향, 비용
 
     def calculate_cost(direction, prev_direction, cost):
-        if prev_direction == -1 or (prev_direction - direction) % 2 == 0:
+        if (prev_direction - direction) % 2 == 0:
             return cost + 100
         else:
             return cost + 600
@@ -72,10 +30,10 @@ def solution(board):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if 0 <= nx < n and 0 <= ny < n and board[nx][ny] == 0:
+            if 0 <= nx < n and 0 <= ny < n and board[nx][ny] == 0:  # 유효한 범위에 있고, 도로를 세울 수 있는 영역일 경우
                 next_cost = calculate_cost(i, direction, cost)
-                if distance[nx][ny][i] > next_cost:
-                    distance[nx][ny][i] = next_cost
-                    q.append((nx, ny, i, next_cost))
+                if distance[nx][ny][i] > next_cost:  # 아직 방문하지 않았거나 지금 계산한 비용이 기존 최소 비용보다 더 작다면,
+                    distance[nx][ny][i] = next_cost   # distance 배열 갱신 후
+                    q.append((nx, ny, i, next_cost))   # 새로운 후보지를 큐에 추가한다.
 
     return min(distance[n - 1][n - 1])
