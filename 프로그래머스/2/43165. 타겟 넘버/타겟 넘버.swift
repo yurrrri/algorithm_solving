67fbc803d0a6f2dd32801473e5db66587abe2a34
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-05-08 17:25:31 +0900

    [level 2] Title: 타겟 넘버, Time: 42.94 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.swift
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.swift
@@ -4,8 +4,8 @@ func solution(_ numbers:[Int], _ target:Int) -> Int {
     var answer = 0
     
     func dfs(_ sum: Int, _ depth: Int) {
-        if depth == numbers.count {
-            if sum == target {
+        if depth == numbers.count {  // 모든 정수들을 활용해야하므로 depth가 numbers의 길이와 같아야하며, 이를 넘어서면 안되므로 return 처리함
+            if sum == target {     // 이때 target과 같으면 저장
                 answer += 1
             }
             return
