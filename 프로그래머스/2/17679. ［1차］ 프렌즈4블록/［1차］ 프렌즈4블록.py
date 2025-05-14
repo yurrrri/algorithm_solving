b168b8fb5417e434c1db679d372d18db285bfc20
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-05-14 20:56:39 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 329.66 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
@@ -6,26 +6,24 @@ def solution(m, n, board):
     
     def down():
         for y in range(n):
-            for i in range(m-2, -1, -1):
-                for x in range(m-1, i, -1):
+            for i in range(m-2, -1, -1):   # 아래
+                for x in range(m-1, i, -1):   # 위
                     if board[i][y] != "." and board[x][y] == ".":
                         board[x][y] = board[i][y]
                         board[i][y] = "."
     
     while True:
-        pop_set = set()
+        pop_set = set()   # 사라질때 겹치는 좌표가 있을 수 있으므로 중복 제거를 위해 set 사용
 
         # 1. 2x2 같은 블록 찾기
         for i in range(m):
             for j in range(n):
                 if not (0<=i+1<m and 0<=j+1<n):
                     continue
-                if board[i][j] == ".":
-                    continue
-                if board[i][j] == board[i+1][j] == board[i][j+1] == board[i+1][j+1]:
+                if board[i][j] == board[i+1][j] == board[i][j+1] == board[i+1][j+1] and board[i][j] != ".":
                     pop_set.update([(i, j), (i+1, j), (i, j+1), (i+1, j+1)])
 
-        # 2. 제거 없으면 종료
+        # 2. 제거할 블록이 없으면 멈춤
         if not pop_set:
             break
         answer += len(pop_set)
