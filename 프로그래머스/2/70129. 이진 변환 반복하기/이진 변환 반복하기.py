05c90fac0734fa643815e80b53503dd11fba9ed3
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-05-18 18:29:00 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 1.02 ms, Memory: 9.43 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,19 +1,30 @@
+# def solution(s):
+#     bin_count = 0
+#     zero_count = 0
+    
+#     def s_to_bin(n):
+#         answer = ""
+#         while n != 0:
+#             answer += str(n%2)
+#             n = n//2
+#         return answer[::-1]
+    
+#     while s != "1":
+#         bin_count += 1
+#         zero_count += s.count("0")
+#         while "0" in s:
+#             s = s.replace("0", "")
+#         length = len(s)
+#         s = s_to_bin(length)
+#     return [bin_count, zero_count]
+
 def solution(s):
     bin_count = 0
     zero_count = 0
     
-    def s_to_bin(n):
-        answer = ""
-        while n != 0:
-            answer += str(n%2)
-            n = n//2
-        return answer[::-1]
-    
-    while s != "1":
-        bin_count += 1
-        zero_count += s.count("0")
-        while "0" in s:
-            s = s.replace("0", "")
-        length = len(s)
-        s = s_to_bin(length)
+    while s != "1":       # s가 1이 될 때까지 반복
+        bin_count += 1       # 이진 변환을 수행한 횟수
+        zero_count += s.count("0")   # 변환 과정에서 제거된 모든 0의 갯수
+        s = bin(s.count("1"))[2:]   # 1의 갯수를 세고, 해당 수를 이진수로 변환
+        
     return [bin_count, zero_count]
