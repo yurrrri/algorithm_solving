70c24a19aebcdd996364f742760a1e4648bae049
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-05-18 19:39:49 +0900

    [level 2] Title: 카펫, Time: 310.37 ms, Memory: 9.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,7 +1,6 @@
 def solution(brown, yellow):
     _sum = brown + yellow
-    for i in range(1, brown+1):
-        for j in range(1, brown+1):
+    for i in range(3, brown+1):   # 세로
+        for j in range(3, brown+1):    # 가로
             if i*j == _sum and (i-2)*(j-2) == yellow:
                 return [j, i]
-            
