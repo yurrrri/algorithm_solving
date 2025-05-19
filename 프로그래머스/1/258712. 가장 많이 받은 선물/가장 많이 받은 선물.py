dbed48f7b05ae5eae543c857353ef86ad1e60548
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-05-19 20:55:49 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 6.98 ms, Memory: 9.71 MB -BaekjoonHub

---
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -0,0 +1,38 @@
+from collections import defaultdict
+
+def solution(friends, gifts):
+    present_count = defaultdict(int)
+    dic1 = defaultdict(dict)    # 선물을 준 기록 딕셔너리
+    dic2 = {}    # 각 프렌즈마다 준 선물, 받은 선물 갯수를 기록하는 딕셔너리
+    for friend1 in friends:
+        for friend2 in friends:
+            if friend1 != friend2:
+                dic1[friend1][friend2] = 0
+                dic2[friend1] = [0, 0]
+
+    for gift in gifts:
+        giver, receiver = gift.split()
+        dic1[giver][receiver] += 1
+        dic2[giver][0] += 1
+        dic2[receiver][1] += 1
+        
+    for friend1 in friends:
+        for (friend2, v) in dic1[friend1].items():
+            if dic1[friend1][friend2] != 0 or dic1[friend2][friend1] != 0:
+                if dic1[friend1][friend2] > dic1[friend2][friend1]:
+                    present_count[friend1] += 1
+                elif dic1[friend1][friend2] == dic1[friend2][friend1]:
+                    friend1_present_num = dic2[friend1][0] - dic2[friend1][1]
+                    friend2_present_num = dic2[friend2][0] - dic2[friend2][1]
+                    
+                    if friend1_present_num > friend2_present_num:
+                        present_count[friend1] += 1
+            elif dic1[friend1][friend2] == 0 and dic1[friend2][friend1] == 0:
+                    friend1_present_num = dic2[friend1][0] - dic2[friend1][1]
+                    friend2_present_num = dic2[friend2][0] - dic2[friend2][1]
+                    
+                    if friend1_present_num > friend2_present_num:
+                        present_count[friend1] += 1
+             
+            
+    return max(list(present_count.values())) if list(present_count.values()) else 0
