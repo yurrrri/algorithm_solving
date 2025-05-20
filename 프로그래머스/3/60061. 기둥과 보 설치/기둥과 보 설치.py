8a1300986160b4dfd3848acadfe9af15dd97a63d
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-05-20 21:58:16 +0900

    [level 3] Title: 기둥과 보 설치, Time: 69.10 ms, Memory: 9.49 MB -BaekjoonHub

--- 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.py
+++ 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.py
@@ -2,16 +2,14 @@ def solution(n, build_frame):
     
     def isValid():
         for x, y in gidung:
-            if y == 0 or (x, y-1) in gidung or (x-1, y) in bo or (x, y) in bo:
-                continue
-            return False
+            # 바닥 위에 있거나, 다른 기둥 위에 있거나, 보의 한쪽 끝부분에 있거나
+            if not (y == 0 or (x, y-1) in gidung or (x-1, y) in bo or (x, y) in bo):
+                return False
 
         for x, y in bo:
-            if (x, y-1) in gidung or (x+1, y-1) in gidung:
-                continue
-            if (x-1, y) in bo and (x+1, y) in bo:
-                continue
-            return False
+            # 한쪽 끝부분이 기둥 위에 있거나, 양쪽 끝부분이 다른 보와 연결되어 있어야함
+            if not ((x, y-1) in gidung or (x+1, y-1) in gidung or (x+1, y) in bo and (x-1, y) in bo):
+                return False
 
         return True
     
