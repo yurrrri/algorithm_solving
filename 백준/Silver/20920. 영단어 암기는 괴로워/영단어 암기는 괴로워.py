287eed466e151f56b2e005cce497a4873de5c410
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-06-09 18:08:12 +0900

    Update 영단어 암기는 괴로워.py

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
@@ -15,7 +15,8 @@ for _ in range(n):
 
 counter = Counter(arr)
 arr = set(arr)
-arr = list(arr)
-
 arr = sorted(arr, key=lambda x:(-counter[x], -len(x), x))
+# 1. 자주나오는 단어 --> counter value값 내림차순
+# 2. 해당 단어의 길수록 앞에 배치 --> len값 기준 내림차순
+# 3. 알파벳 사전 순으로 앞에 있는 단어 --> 사전 순 오름차순
 print("\n".join(arr))
