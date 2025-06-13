0997a11c6710b03f5663d1a0823030bfa80dcff9
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-06-14 00:10:06 +0900

    [level 2] Title: 큰 수 만들기, Time: 89.31 ms, Memory: 19.7 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,19 +1,15 @@
 def solution(number, k):
     answer = ''
-    stack = []
+    stack = [number[0]]
     number = list(number)
     
-    for n in number:
-        if len(stack) == 0:
-            stack.append(n)
-            continue
-        
-        while stack and stack[-1] < n and k > 0:
+    for n in number[1:]:
+        while stack and stack[-1] < n and k:
             k -= 1
             stack.pop()
         stack.append(n)
     
-    if k > 0:
+    if k > 0:   # k를 모두 쓰지않았을 경우
         stack = stack[:len(number)-k]
             
     return "".join(stack)
