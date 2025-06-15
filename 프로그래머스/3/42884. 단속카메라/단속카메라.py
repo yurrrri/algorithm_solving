818632b5a3b42439bbb380b5e37051d97949a203
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-06-15 23:29:22 +0900

    [level 3] Title: 단속카메라, Time: 0.86 ms, Memory: 9.53 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,10 +1,11 @@
 def solution(routes):
-    routes.sort(key = lambda x : x[1])
+    routes.sort(key=lambda x: x[1])  # 진출 지점 기준 정렬
+    prev_camera = -30001  # 이전에 설치한 카메라 위치
     answer = 0
-    prev_end = -30001
-    for start, end in routes :
-        if start > prev_end :
-            answer += 1
-            prev_end = end
+
+    for start, end in routes:
+        if start > prev_camera:
+            answer += 1          # 새 카메라 필요
+            prev_camera = end    # 현재 차량의 진출 지점에 카메라 설치
 
     return answer
