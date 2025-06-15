0f041381b38342acf032750166f62eedc19e6ecb
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-06-15 22:19:38 +0900

    [level 3] Title: 기지국 설치, Time: 4.90 ms, Memory: 9.48 MB -BaekjoonHub

---
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
@@ -0,0 +1,14 @@
+def solution(n, stations, w):
+    idx = 0     # 기지국이 설치되어있는 인덱스
+    location = 1   # 현재 위치
+    answer = 0
+    
+    while location <= n:
+        if idx < len(stations) and location >= stations[idx] - w:  # 전파가 닿아있는 범위를 보면 건너뛰어야함
+            location = stations[idx] + w + 1
+            idx += 1
+        else:     # 닿아있지 않다면 전파 설치
+            answer += 1
+            location += 2 * w + 1
+            
+    return answer
