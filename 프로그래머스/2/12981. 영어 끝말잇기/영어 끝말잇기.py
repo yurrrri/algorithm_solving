d75b188fac2c6370f5b3e75627839be741bf9adc
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-06-15 17:01:04 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.04 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -2,10 +2,10 @@ def solution(n, words):
     word_set = set()
     
     for i, word in enumerate(words):
-        if word in word_set:
+        if word in word_set:    # 이미 있는 단어를 말할 때 탈락
             # print(word)
             return [(i%n)+1, (i//n)+1]
-        if i != 0 and word[0] != words[i-1][-1]:
+        if i != 0 and word[0] != words[i-1][-1]:   # 앞 단어의 뒷글자와 현재 말하는 단어의 앞글자가 다를때 탈락
             # print(word)
             return [(i%n)+1, (i//n)+1]
         word_set.add(word)
