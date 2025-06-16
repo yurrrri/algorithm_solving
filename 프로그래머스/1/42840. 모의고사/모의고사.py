e354a1207e0998daf3c9671c7da8ad10f3308fc2
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-06-16 13:43:05 +0900

    [level 1] Title: 모의고사, Time: 5.39 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -3,22 +3,22 @@ def solution(answers):
     two = [2, 1, 2, 3, 2, 4, 2, 5]
     three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     
-    one_score, two_score, three_score = (0, 0, 0)
+    scores = [0] * 3
     for i, answer in enumerate(answers):
         j = i%len(one)
         k = i%len(two)
         l = i%len(three)
         
         if answer == one[j]:
-            one_score += 1
+            scores[0] += 1
             
         if answer == two[k]:
-            two_score += 1
+            scores[1] += 1
             
         if answer == three[l]:
-            three_score += 1
+            scores[2] += 1
             
-    max_score = max(one_score, two_score, three_score)
-    answer = [r[0] for r in [(1, one_score), (2, two_score), (3, three_score)] if r[1] == max_score]
+    max_score = max(scores)
+    answer = [i+1 for i, score in enumerate(scores) if score == max_score]
             
     return answer
