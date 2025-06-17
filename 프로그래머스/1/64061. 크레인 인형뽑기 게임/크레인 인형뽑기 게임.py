6247a99fd9d057a34ea96f8e12dc832605cac584
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-06-17 23:52:08 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 2.63 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -1,21 +1,16 @@
-def isEmpty(stack):
-    return len(stack) == 0
-
 def solution(board, moves):
     answer = 0
-    board_size = len(board)
     stack = []
+    n = len(board)
     
-    for move in moves:
-        for i in range(board_size):
-            doll = board[i][move-1]  # 인형 찾아서
-            board[i][move-1] = 0    # 꺼냄
-            if doll != 0:   # 인형 찾음, 없는 곳에서 크레인을 작동시키면 아무일도 일어나지 않음
-                if not isEmpty(stack) and stack[-1] == doll: # 마지막께 넣는 인형이랑 같으면 터지고 갯수 더함
-                    answer += 2   # 같이 없어지므로 + 2
+    for num in moves:
+        for i in range(n):
+            if board[i][num-1] != 0:
+                if stack and stack[-1] == board[i][num-1]:
+                    answer += 2
                     stack.pop()
-                else:    # 그게 아니면 인형 계속 추가하기
-                    stack.append(doll)
-                break     # 0이 아닌 인형을 찾으면 그 인형만 가지고 그 다음 스텝으로 넘어가야하므로 반복문 벗어남
-                 
+                else:
+                    stack.append(board[i][num-1])
+                board[i][num-1] = 0
+                break
     return answer
