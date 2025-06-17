97ebbd163aedda0f1c5056e60263e59e11e5e82c
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-06-17 22:17:15 +0900

    [level 2] Title: 주식가격, Time: 25.53 ms, Memory: 19 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,17 +1,14 @@
 def solution(prices):
-    n = len(prices)
-    answer = [0] * n      # 길이를 저장할 배열
-    stack = []        # 뒤에서부터 비교하므로 스택 사용
-    stack.append(0)   # 처음은 비교대상이 없으므로 먼저 0 푸시해줌
+    answer = [0] * len(prices)
+    stack = [0]
     
-    for i in range(1, n):
-        while stack and prices[i] < prices[stack[-1]]: # 그 전에 있는 애랑 비교해서 더 크면 길이 확정지움
+    for i in range(1, len(prices)):
+        while stack and prices[stack[-1]] > prices[i]:
             j = stack.pop()
             answer[j] = i - j
         stack.append(i)
-        
+                
     while stack:
-        j = stack.pop()
-        answer[j] = n-1-j
-        
+        idx = stack.pop()
+        answer[idx] = len(prices) - idx - 1
     return answer
