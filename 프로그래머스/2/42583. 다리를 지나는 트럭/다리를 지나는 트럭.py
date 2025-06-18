3b79d2b17b7e657d2e50681fc71dbc467c56cb0e
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-06-18 22:36:57 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 48.76 ms, Memory: 9.35 MB -BaekjoonHub

---
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+def solution(bridge_length, weight, truck_weights):
+    
+    answer = 0
+    bridge = deque([0] * bridge_length)  # [0]*bridge_length 을 덱으로 변환
+    truck_weights = deque(truck_weights) # 리스트를 덱으로 변환
+    
+    currentWeight = 0
+    while truck_weights:
+        answer += 1
+        currentWeight -= bridge.popleft()
+
+        if currentWeight + truck_weights[0] <= weight:
+            currentWeight += truck_weights[0]
+            bridge.append(truck_weights.popleft())
+
+        else: 
+            bridge.append(0)
+            
+    answer += bridge_length
+    
+    return answer
