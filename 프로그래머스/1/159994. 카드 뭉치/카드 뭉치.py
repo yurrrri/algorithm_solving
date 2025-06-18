e58ebe8404f388fcad3facb0400d70c153ef3718
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-06-18 16:13:28 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -1,18 +1,20 @@
 from collections import deque
 
 def solution(cards1, cards2, goal):
+    answer = ''
+    
     cards1 = deque(cards1)
     cards2 = deque(cards2)
     goal = deque(goal)
     
-    while goal:     # goal의 원소가 남아있을동안
-        if cards1 and cards1[0] == goal[0]:    
+    while goal:
+        if cards1 and cards1[0] == goal[0]:
+            goal.popleft()
             cards1.popleft()
-            goal.popleft()
         elif cards2 and cards2[0] == goal[0]:
-            cards2.popleft()
             goal.popleft()
-        else:      # 앞의 순서가 맞지 않으면 만들지 못하므로 바로 return No
+            cards2.popleft()
+        else:
             return "No"
-        
+    
     return "Yes"
