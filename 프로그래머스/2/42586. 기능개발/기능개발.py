b4a01b6b0e518eb20f5d8abda04e2b2fffdaac01
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-06-18 15:57:28 +0900

    [level 2] Title: 기능개발, Time: 0.58 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,21 +1,20 @@
-from collections import deque
-
 def solution(progresses, speeds):
     answer = []
-    progresses_q = deque(progresses)
-    speeds_q = deque(speeds)
     
-    while progresses_q:
-        while progresses_q[0] < 100:
-            for i in range(len(progresses_q)):
-                progresses_q[i] += speeds_q[i]
+    while progresses:
+        for i in range(len(progresses)):
+            progresses[i] += speeds[i]
                 
         _sum = 0
-        while progresses_q and progresses_q[0] >= 100:
-            _sum += 1
-            progresses_q.popleft()
-            speeds_q.popleft()
-            
-        answer.append(_sum)
+        while progresses:
+            if progresses[0] >= 100:
+                _sum += 1
+                progresses.pop(0)
+                speeds.pop(0)
+            else:
+                break
+                
+        if _sum:
+            answer.append(_sum)
         
     return answer
