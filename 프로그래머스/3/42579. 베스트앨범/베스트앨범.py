8079765a4b7f85b7b162ec9d9ec5f498b077c838
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-06-19 15:56:36 +0900

    [level 3] Title: 베스트앨범, Time: 0.06 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,19 +1,18 @@
+from collections import defaultdict
+
 def solution(genres, plays):
-    dic = {}
-    count_dic = {}
     answer = []
-    
-    for i in range(len(genres)):
-        if not genres[i] in dic:
-            dic[genres[i]] = []
-            count_dic[genres[i]] = 0
-        dic[genres[i]].append((i, plays[i]))
-        count_dic[genres[i]] += plays[i]
+    play_total_dic = defaultdict(int)      # 장르 별 총 재생횟수를 저장할 딕셔너리
+    genre_play_dic = defaultdict(list)      # 장르별 (고유번호, 재생횟수) 튜플을 저장할 딕셔너리
+    for i, (genre, play) in enumerate(zip(genres, plays)):
+        play_total_dic[genre] += play
+        genre_play_dic[genre].append((i, play))
         
-    sorted_genres = sorted(count_dic.items(), key=lambda x:x[1], reverse=True)
+    sorted_play_total = sorted(play_total_dic, key=lambda x:-play_total_dic[x])
     
-    for genre, _ in sorted_genres:
-        temp = sorted(dic[genre], key=lambda x:(-x[1], x[0]))
-        answer.extend([idx for idx, _ in temp[:2]])
-            
+    for genre in sorted_play_total:
+        _sorted = sorted(genre_play_dic[genre], key=lambda x:(-x[1], x[0]))[:2]
+        for i, _ in _sorted:
+            answer.append(i)
+
     return answer
