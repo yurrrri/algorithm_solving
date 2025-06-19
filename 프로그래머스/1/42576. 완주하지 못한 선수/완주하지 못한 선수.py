a393c78e555182d22ee2742c7da8586e04b2ed77
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-06-19 15:13:53 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 89.67 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,21 +1,12 @@
+from collections import Counter
+
 def solution(participant, completion):
-    dic = {}
-    for part in participant:
-        if part not in dic.keys():
-            dic[part] = 1
-        else:
-            dic[part] += 1
-                    
-    for c in completion:
-        dic[c] -= 1
-                
-    for key, value in dic.items():
-        if value != 0:
-            return key
-
-# import collections
-
-# def solution(participant, completion):
-#     answer = collections.Counter(participant) - collections.Counter(completion)  # value가 0인 key-value쌍 제거되어 반환
-#     print(list(answer.values()))
-#     return list(answer.keys())[0]
+    answer = ''
+    counter = Counter(participant)
+    
+    for player in completion:
+        counter[player] -= 1
+        if counter[player] == 0:
+            del counter[player]
+        
+    return list(counter.keys())[-1]
