9b8c96f72f70922a55659a25d82775dde16aa754
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-06-19 15:22:27 +0900

    [level 2] Title: 할인 행사, Time: 149.91 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,29 +1,14 @@
 from collections import Counter
 
 def solution(want, number, discount):
-    want_dic = {}
     answer = 0
-    flag = False
-    
-    for i in range(len(want)):
-        want_dic[want[i]] = number[i]
+    want_dic = {}
+    for i, w in enumerate(want):
+        want_dic[w] = number[i]
         
-    for i in range(len(discount)):  # 할인행사하는 맨 마지막날에 회원가입할수도 있으므로 discount 길이 전체 순회
-        flag = False
-        
-        if i+10 >= len(discount):
-            k = len(discount)
-        else:
-            k = i + 10
-        
-        dic = Counter(discount[i:k])
-                        
-        for w in want:   # 항목에 없거나 살 수 있는 수량이 부족하면 break
-            if w not in dic or dic[w] < want_dic[w]:
-                flag = True
-                break
-            
-        if not flag:
+    for i in range(len(discount)):
+        counter = Counter(discount[i:i+10])
+        if want_dic == counter:
             answer += 1
         
     return answer
