f29c6bf6eed76deab019c4050f969deb166395bf
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-06-23 19:24:43 +0900

    [level 3] Title: 네트워크, Time: 0.34 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -4,22 +4,17 @@ def solution(n, computers):
     answer = 0
     visited = [False] * n
     
-    def bfs(v):
-        q = deque([v])
+    def dfs(v):
         visited[v] = True
         
-        while q:
-            node = q.popleft()
-        
-            for i in range(n):
-                if i != node and not visited[i] and computers[node][i] == 1:
-                    q.append(i)
-                    visited[i] = True
+        for i in range(n):
+            if not visited[i] and computers[v][i] == 1:
+                dfs(i)
         
         
     for i in range(n):
         if not visited[i]:
-            bfs(i)
+            dfs(i)
             answer += 1
         
     return answer
