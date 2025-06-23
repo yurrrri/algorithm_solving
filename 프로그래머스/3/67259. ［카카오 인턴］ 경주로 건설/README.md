608a81a3c116d1b59f0fe8b8e0916dd5cbac3a53
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-06-23 21:14:14 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 44.77 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/README.md
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/README.md
@@ -1,10 +1,10 @@
 # [level 3] [카카오 인턴] 경주로 건설 - 67259 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67259?language=swift) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67259?language=python3) 
 
 ### 성능 요약
 
-메모리: 16.4 MB, 시간: 4.96 ms
+메모리: 9.35 MB, 시간: 44.77 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 08일 20:20:42
+2025년 06월 23일 21:14:10
 
 ### 문제 설명
 
--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -1,39 +1,39 @@
 from collections import deque
 
 def solution(board):
+    answer = 0
     n = len(board)
-    INF = 1e9
-    
-    # 방향: 상(0), 좌(1), 하(2), 우(3)
-    dx = [-1, 0, 1, 0]
+    dx = [-1, 0, 1, 0]   # 상-좌-하-우 offset
     dy = [0, -1, 0, 1]
-
-    # 3차원 거리 배열: [x][y][dir]
-    distance = [[[INF] * 4 for _ in range(n)] for _ in range(n)]
-
-    # 초기 위치에서 네 방향 모두 시작 가능하므로 모든 후보지에 대한 distance 배열 초기화 및 큐에 추가한다.
+    visited = [[[-1] * 4 for _ in range(n)] for _ in range(n)]
     q = deque([])
     for i in range(4):
-        distance[0][0][i] = 0
-        q.append((0, 0, i, 0))  # x, y, 방향, 비용
-
-    def calculate_cost(direction, prev_direction, cost):
-        if (prev_direction - direction) % 2 == 0:
-            return cost + 100
+        q.append((0, 0, i, 0))
+        visited[0][0][i] = 0
+    
+    def is_valid(x, y):
+        if 0<=x<n and 0<=y<n and board[x][y] == 0:
+            return True
         else:
-            return cost + 600
-
+            return False
+    
+    def calculate_cost(prev, current):
+        if prev == -1 or abs(prev-current) % 2 == 0:
+            return 100
+        else:
+            return 600
+        
     while q:
-        x, y, direction, cost = q.popleft()
+        x, y, prev_dir, cost = q.popleft()
 
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-
-            if 0 <= nx < n and 0 <= ny < n and board[nx][ny] == 0:  # 유효한 범위에 있고, 도로를 세울 수 있는 영역일 경우
-                next_cost = calculate_cost(i, direction, cost)
-                if distance[nx][ny][i] > next_cost:  # 아직 방문하지 않았거나 지금 계산한 비용이 기존 최소 비용보다 더 작다면,
-                    distance[nx][ny][i] = next_cost   # distance 배열 갱신 후
-                    q.append((nx, ny, i, next_cost))   # 새로운 후보지를 큐에 추가한다.
-
-    return min(distance[n - 1][n - 1])
+            
+            new_cost = cost + calculate_cost(prev_dir, i)
+            
+            if is_valid(nx, ny) and (visited[nx][ny][i] == -1 or visited[nx][ny][i] > new_cost):
+                visited[nx][ny][i] = new_cost
+                q.append((nx, ny, i, new_cost))
+    
+    return min([c for c in visited[n-1][n-1] if c != -1])
