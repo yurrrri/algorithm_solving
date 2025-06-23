1d925a10f94b141dae57c0c139b8fd565e899f79
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-06-23 21:35:24 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 42.35 ms, Memory: 9.41 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -5,8 +5,9 @@ def solution(board):
     n = len(board)
     dx = [-1, 0, 1, 0]   # 상-좌-하-우 offset
     dy = [0, -1, 0, 1]
-    visited = [[[-1] * 4 for _ in range(n)] for _ in range(n)]
-    q = deque([])
+    INF = int(1e9)
+    visited = [[[INF] * 4 for _ in range(n)] for _ in range(n)]
+    q = deque()
     for i in range(4):
         q.append((0, 0, i, 0))
         visited[0][0][i] = 0
@@ -14,8 +15,7 @@ def solution(board):
     def is_valid(x, y):
         if 0<=x<n and 0<=y<n and board[x][y] == 0:
             return True
-        else:
-            return False
+        return False
     
     def calculate_cost(prev, current):
         if prev == -1 or abs(prev-current) % 2 == 0:
@@ -32,8 +32,8 @@ def solution(board):
             
             new_cost = cost + calculate_cost(prev_dir, i)
             
-            if is_valid(nx, ny) and (visited[nx][ny][i] == -1 or visited[nx][ny][i] > new_cost):
+            if is_valid(nx, ny) and visited[nx][ny][i] > new_cost:
                 visited[nx][ny][i] = new_cost
                 q.append((nx, ny, i, new_cost))
     
-    return min([c for c in visited[n-1][n-1] if c != -1])
+    return min(visited[n-1][n-1])
