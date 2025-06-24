6493831c917010ec35583fbc75d7699d8fdc0aeb
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-06-24 19:23:37 +0900

    [level 4] Title: 지형 이동, Time: 561.17 ms, Memory: 20.7 MB -BaekjoonHub

--- 프로그래머스/4/62050. 지형 이동/지형 이동.py
+++ 프로그래머스/4/62050. 지형 이동/지형 이동.py
@@ -8,6 +8,10 @@ def solution(land, height):
     
     visited = [[False] * n for _ in range(n)]
     
+    """
+    우선순위 큐 사용 이유 --> 사다리 설치 비용을 최소로 한다는것은 인접한 칸끼리의 비용이 최소인 순서대로 방문해야하기 때문에
+    pop을 할때마다 최소값을 보장해주는 우선순위 큐 사용
+    """
     q = []
     heapq.heappush(q, (0, 0, 0))
     
@@ -17,8 +21,8 @@ def solution(land, height):
         if visited[x][y]:
             continue
             
-        answer += cost
-        visited[x][y] = True
+        answer += cost            # 사다리를 설치했다면 차이 비용일 것이고, 아니라면 0일 것임 (영향 X)
+        visited[x][y] = True      # 최소 비용으로 선택한 다음 노드에 대해 방문처리
         
         for i in range(4):
             nx = x + dx[i]
