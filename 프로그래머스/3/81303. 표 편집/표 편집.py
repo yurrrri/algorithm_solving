532ef95d7e6e8540745306c6cb7dcc7000ad6b7d
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-06-24 19:26:05 +0900

    [level 3] Title: 표 편집, Time: 260.40 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.py
+++ 프로그래머스/3/81303. 표 편집/표 편집.py
@@ -1,42 +1,36 @@
 def solution(n, k, cmd):
-  # ➊ 삭제된 행의 인덱스를 저장하는 리스트
-  deleted = [ ]
+    stack = []   # 삭제된 인덱스를 저장하는 리스트 (가장 최근에 삭제된 행으로 연산하므로 스택 활용)
 
-  # ➋ 링크드 리스트에서 각 행 위아래의 행의 인덱스를 저장하는 리스트
-  up = [i - 1 for i in range(n + 2)]
+    # 각 행 위아래의 행의 인덱스를 저장하는 리스트
+    up = [i - 1 for i in range(n + 2)]
+    down = [i + 1 for i in range(n + 1)]
 
-  down = [i + 1 for i in range(n + 1)]
+    k += 1    # 위 아래로 임시공간을 늘렸으므로, 초기에 1을 더한 것이 처음 시작임
 
-  # ➌ 현재 위치를 나타내는 인덱스
-  k += 1
+    for c in cmd:
+        if c.startswith("C"):     # C: 행 삭제
+            stack.append(k)       # 삭제한 인덱스 추가
+            up[down[k]] = up[k]
+            down[up[k]] = down[k]
+            k = up[k] if n < down[k] else down[k]
 
-  # ➍ 주어진 명령어(cmd) 리스트를 하나씩 처리
-  for cmd_i in cmd:
-    # ➎ 현재 위치를 삭제하고 그다음 위치로 이동
-    if cmd_i.startswith("C"):
-      deleted.append(k)
-      up[down[k]] = up[k]
-      down[up[k]] = down[k]
-      k = up[k] if n < down[k] else down[k]
+        elif c.startswith("Z"):     # 가장 최근에 삭제한 행 복구
+            restore = stack.pop() 
+            down[up[restore]] = restore
+            up[down[restore]] = restore
 
-    # ➏ 가장 최근에 삭제된 행을 복원
-    elif cmd_i.startswith("Z"):
-      restore = deleted.pop( ) 
-      down[up[restore]] = restore
-      up[down[restore]] = restore
+        # U 또는 D: 위 혹은 아래로 이동
+        else:
+            action, num = c.split( ) 
+            if action == "U":
+                for _ in range(int(num)):
+                    k = up[k]
+            else:
+                for _ in range(int(num)):
+                    k = down[k]
 
-    # ➐ U 또는 D를 사용해 현재 위치를 위아래로 이동
-    else:
-      action, num = cmd_i.split( ) 
-      if action == "U":
-        for _ in range(int(num)):
-          k = up[k]
-      else:
-        for _ in range(int(num)):
-          k = down[k]
-
-  # ➑ 삭제된 행의 위치에 'X'를, 그렇지 않은 행의 위치에 'O'를 포함하는 문자열 반환
-  answer = ["O"] * n
-  for i in deleted:
-    answer[i - 1] = "X"
-  return "".join(answer)
+    answer = ["O"] * n
+    for i in stack:
+        answer[i - 1] = "X"
+    
+    return "".join(answer)
