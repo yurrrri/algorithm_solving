933b1aaa98e62964aa91824fa920ebccaeb091e9
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-06-24 16:26:10 +0900

    [level 3] Title: 여행경로, Time: 307.22 ms, Memory: 13.5 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -3,15 +3,15 @@ def solution(tickets):
     visited = [False] * len(tickets)
     
     def dfs(v, arr):
-        if len(arr) == len(tickets) + 1:
+        if len(arr) == len(tickets) + 1:   # 주어진 항공권은 모두 사용해야한다
             answer.append(arr)
             return
         
         for i, [start, end] in enumerate(tickets):
             if start == v and not visited[i]:
-                visited[i] = True
+                visited[i] = True     # 해당 티켓을 사용했다가
                 dfs(end, arr + [end])
-                visited[i] = False
+                visited[i] = False    # 복원
                 
     dfs("ICN", ["ICN"])
     answer.sort()
