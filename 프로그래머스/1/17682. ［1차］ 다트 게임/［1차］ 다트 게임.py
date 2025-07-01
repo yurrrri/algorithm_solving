08890db6455446eda1a864494382aad3732878a2
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-07-02 01:13:46 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.10 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -1,7 +1,8 @@
 def solution(dartResult):
     answer = 0
     stack = []
-    dartResult = dartResult.replace('10', 'k')
+    dartResult = dartResult.replace('10', 'k')         # 10을 처리하기 위해 k로 바꾼다음, 아래에서 다시 10으로 바꿔줌
+    # 이렇게 처리하기 까다로운 문자열이 포함되어있으면, replace를 활용하는 스킬을 떠올리자
     dartResult = ['10' if r == 'k' else r for r in dartResult]
         
     for dart in dartResult:
@@ -12,7 +13,7 @@ def solution(dartResult):
         elif dart == "T":
             stack[-1] = stack[-1] ** 3
         elif dart == "*":
-            stack[-2:] = [r * 2 for r in stack[-2:]]
+            stack[-2:] = [r * 2 for r in stack[-2:]]      # 어차피 이전 점수가 없어도 리스트 슬라이싱은 인덱스 오류가 발생하지 않음
         elif dart == "#":
             stack[-1] *= (-1)
         else:
