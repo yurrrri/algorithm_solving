e54a9dc96b1db102be809ec51129db1dbe095f2d
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-07-02 17:19:06 +0900

    [level 2] Title: 롤케이크 자르기, Time: 668.46 ms, Memory: 50.6 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -2,18 +2,16 @@ from collections import Counter
 
 def solution(topping):
     answer = 0
-
-    old = set()
-    young = Counter(topping)
+    s1 = set()
+    s2 = Counter(topping)
     
     for t in topping:
-        old.add(t)
-        young[t] -= 1
-        
-        if young[t] == 0:
-            young.pop(t)
+        s1.add(t)
+        s2[t] -= 1
+        if s2[t] == 0:
+            del s2[t]
             
-        if len(old) == len(young):
+        if len(s1) == len(s2):
             answer += 1
     
     return answer
