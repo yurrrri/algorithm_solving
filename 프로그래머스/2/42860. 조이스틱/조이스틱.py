b0564d26bd064e9c75f7420aad88a6c43cb8f35c
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-07-02 18:49:42 +0900

    [level 2] Title: 조이스틱, Time: 0.02 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -2,18 +2,21 @@ def solution(name):
     answer = 0
     n = len(name)
 
-    for ch in name:
+    for ch in name:       # 1)
         if (ch != 'A'):
             min_up_down = min(ord(ch) - ord('A'), 26 + ord('A') - ord(ch))
             answer += min_up_down
-
-    move = n - 1
-    for idx in range(n):
-        next_idx = idx + 1
-        while (next_idx < n) and (name[next_idx] == 'A'):
-            next_idx += 1
-        distance = min(idx, n - next_idx)
-        move = min(move, idx + n - next_idx + distance)
+            
+    move = 21          # 2)
+    for left in range(n):
+        idx = left + 1
+        while (idx < n) and (name[idx] == 'A'):
+            idx += 1
+            
+        right = n - idx
+        distance = min(left, right)
+        move = min(move, left + right + distance)
 
     answer += move
+    
     return answer
