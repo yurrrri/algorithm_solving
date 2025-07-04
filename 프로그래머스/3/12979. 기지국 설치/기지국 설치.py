109098220d118fbe43c8242a0613478efea8ec62
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-07-04 16:52:34 +0900

    [level 3] Title: 기지국 설치, Time: 5.30 ms, Memory: 9.52 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
@@ -1,14 +1,15 @@
 def solution(n, stations, w):
-    idx = 0     # 기지국이 설치되어있는 인덱스
-    location = 1   # 현재 위치
     answer = 0
+    aparts = range(1, n+1)   # 1부터 n까지의 배열
+    idx = 1           # 현재 탐색하는 아파트 위치
+    station_idx = 0   # 설치된 기지 위치
     
-    while location <= n:
-        if idx < len(stations) and location >= stations[idx] - w:  # 전파가 닿아있는 범위를 보면 건너뛰어야함
-            location = stations[idx] + w + 1
-            idx += 1
-        else:     # 닿아있지 않다면 전파 설치
+    while idx <= n:
+        if station_idx < len(stations) and idx >= stations[station_idx] - w:   # 이미 설치되어있는 기지국의 전파의 범위에 닿을 경우,
+            idx = stations[station_idx] + w + 1   # 전파되지 않은 범위로 이동
+            station_idx += 1     
+        else:               # 이미 설치되어 있는 기지국의 전파 범위가 아닐 경우
+            idx += 2 * w + 1    # 기지국 설치 후, 그 다음 전파가 닿지 않는 범위로 이동
             answer += 1
-            location += 2 * w + 1  # 해당 기지국을 설치함으로써 전파가 닿지않는 범위로 이동
-            
+    
     return answer
