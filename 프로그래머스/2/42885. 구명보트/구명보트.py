751da14ee67100e8f069c24e22f3923017c84eba
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-07-04 13:44:24 +0900

    [level 2] Title: 구명보트, Time: 9.65 ms, Memory: 9.64 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,16 +1,12 @@
 def solution(people, limit):
-    if len(people) == 1:
-        return 1
-    
-    people.sort()
     answer = 0
-    i = 0   # 가장 가벼운 사람을 가리키는 인덱스
-    j = len(people) - 1   # 가장 무거운 사람을 가리키는 인덱스
+    people.sort()
     
+    i, j = 0, len(people)-1
     while i <= j:
         if people[i] + people[j] <= limit:
-            i += 1    # 무거운 사람과 가벼운 사람을 태울 수 있다면 가벼운 사람도 함께 태움
+            i += 1
         answer += 1
-        j -= 1      # 무거운 사람은 항상 보트에 탈 수 있으므로 태움
+        j -= 1
             
     return answer
