6e9073079c8d521ab26cfc3027ac0235e6b2995e
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-07-04 17:16:03 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
 
 ### 성능 요약
 
-메모리: 9.23 MB, 시간: 0.01 ms
+메모리: 9.26 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 15일 20:34:37
+2025년 07월 04일 17:16:01
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,37 +1,13 @@
-# def solution(n, lost, reserve):
-#     answer = 0
-#     lost.sort()
-#     reserve.sort()
-    
-#     for i in range(1, n+1):
-#         if i not in lost: #안 잃어버린 학생
-#             answer += 1
-#         else:
-#             if i in reserve: #잃어버렸지만 여분도 있는 학생
-#                 answer += 1
-#                 reserve.remove(i)
-#                 lost.remove(i)
-
-#     for i in lost: #잃어버리고 여분도 없어서 빌려야 하는 학생
-#         if i-1 in reserve:
-#             answer += 1
-#             reserve.remove(i-1)
-
-#         elif i+1 in reserve:
-#             answer +=1
-#             reserve.remove(i+1)
-
-#     return answer
-
 def solution(n, lost, reserve):
-    _reserve = sorted(set(reserve) - set(lost))
-    _lost = sorted(set(lost) - set(reserve))
+    answer = 0
     
+    _reserve = sorted([r for r in reserve if r not in lost])
+    _lost = sorted([r for r in lost if r not in reserve])
+            
     for r in _reserve:
-        f = r - 1
-        b = r + 1
-        if f in _lost:
-            _lost.remove(f)
-        elif b in _lost:
-            _lost.remove(b)
+        if r-1 in _lost:
+            _lost.remove(r-1)
+        elif r + 1 in _lost:
+            _lost.remove(r+1)
+            
     return n - len(_lost)
