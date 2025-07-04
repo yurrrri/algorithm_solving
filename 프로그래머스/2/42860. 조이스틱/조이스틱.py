3dfb117ce21531e1179da4e3008afb463542c54f
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-07-04 18:35:16 +0900

    [level 2] Title: 조이스틱, Time: 0.03 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -2,19 +2,19 @@ def solution(name):
     answer = 0
     n = len(name)
 
-    for ch in name:       # 1)
+    for ch in name:       # 1) A에서 해당 알파벳까지 바꾸는 데 드는 총 최소 횟수 (각 알파벳마다 위 버튼, 아래 버튼을 누른 횟수를 합쳐서 최소값을 비교함)
         if (ch != 'A'):
             min_up_down = min(ord(ch) - ord('A'), 26 + ord('A') - ord(ch))
             answer += min_up_down
             
-    move = 21          # 2)
+    move = 21          # 2) 좌우로 이동하여 알파벳을 바꾸는 횟수의 최소값
     for left in range(n):
-        idx = left + 1
+        idx = left + 1        # 일단 쭉 오른쪽 커서를 통해 직진한다고 가정하고, 왼쪽 커서로 이동하는 횟수와 비교한다.
         while (idx < n) and (name[idx] == 'A'):
             idx += 1
             
-        right = n - idx
-        distance = min(left, right)
+        right = n - idx      # 해당 위치로 이동하기 위해 왼쪽 커서로 이동하는 경우
+        distance = min(left, right)      # 왼쪽 혹은 오른쪽으로 갔다가, 그 반대방향으로 다시 꺾어서 오는 경우
         move = min(move, left + right + distance)
 
     answer += move
