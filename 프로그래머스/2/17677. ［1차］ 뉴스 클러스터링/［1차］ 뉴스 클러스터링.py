cffb703197cc054ac6a0ccec62bb48f44ea0549e
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-07-07 21:27:20 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.84 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -5,23 +5,10 @@ def solution(str1, str2):
     s1 = [str1[i:i+2].lower() for i in range(len(str1)-1) if str1[i:i+2].isalpha()]
     s2 = [str2[j:j+2].lower() for j in range(len(str2)-1) if str2[j:j+2].isalpha()]
 
-    bunja, bunmo = 0, 0
+    if not s1 and not s2:
+        return 65536
             
     dic1 = Counter(s1)
     dic2 = Counter(s2)
     
-    for key, value in dic1.items():
-        if key in dic2:
-            bunja += min(value, dic2[key])
-            bunmo += max(value, dic2[key])
-        else:
-            bunmo += dic1[key]
-            
-    for key, value in dic2.items():
-        if key not in dic1:
-            bunmo += dic2[key]
-            
-    if bunja == 0 and bunmo == 0:
-        return 65536
-    
-    return int(bunja/bunmo * 65536)
+    return int(sum((dic1 & dic2).values()) / sum((dic1 | dic2).values()) * 65536)
