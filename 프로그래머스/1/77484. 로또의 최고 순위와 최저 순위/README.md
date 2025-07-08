823c81de4cb2a73b46fbf467e0721c9e303f3712
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-07-08 17:12:00 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/README.md
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/README.md
@@ -1,10 +1,10 @@
 # [level 1] 로또의 최고 순위와 최저 순위 - 77484 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77484?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77484) 
 
 ### 성능 요약
 
-메모리: 9.16 MB, 시간: 0.01 ms
+메모리: 9.14 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 08일 17:07:08
+2025년 07월 08일 17:11:58
 
 ### 문제 설명
 
--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -1,21 +1,9 @@
 def solution(lottos, win_nums):
-    answer = [7, 7]
+    rank = [6, 6, 5, 4, 3, 2, 1]
     count = 0
     
     for w in win_nums:
         if w in lottos:
             count += 1
-            
-    answer[1] -= count
-    answer[0] -= count
     
-    have_to_fill = len(win_nums) - count
-    if have_to_fill >= lottos.count(0):
-        answer[0] -= lottos.count(0)
-        
-    if answer[0] == 7:
-        answer[0] = 6
-    if answer[1] == 7:
-        answer[1] = 6
-    
-    return answer
+    return [rank[lottos.count(0) + count], rank[count]]
