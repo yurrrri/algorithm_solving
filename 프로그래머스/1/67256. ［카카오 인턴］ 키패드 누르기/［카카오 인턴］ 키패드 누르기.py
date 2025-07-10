add79045d0197c5bc21f3ea3bf61b04f62e701c2
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-07-10 20:22:44 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.37 ms, Memory: 9.41 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -1,34 +1,66 @@
+# def solution(numbers, hand):
+#     answer = ''
+#     left, right = 10, 12    # *을 10, #을 12로 치환
+#     for i in range(len(numbers)):
+#         if numbers[i] == 0:
+#             numbers[i] = 11
+    
+#     for n in numbers:
+#         if n in [1, 4, 7]:
+#             answer += "L"
+#             left = n
+#         elif n in [3, 6, 9]:
+#             answer += "R"
+#             right = n
+#         elif n in [2, 5, 8, 11]:
+#             left_dist = (abs(n - left) // 3) + abs(n - left) % 3
+#             right_dist = (abs(n - right) // 3) + abs(n - right) % 3
+#             if left_dist < right_dist:
+#                 left = n
+#                 answer += "L"
+#             elif left_dist > right_dist:
+#                 right = n
+#                 answer += "R"
+#             else:
+#                 if hand == "left":
+#                     left = n
+#                     answer += "L"
+#                 else:
+#                     right = n
+#                     answer += "R"
+                    
+#     return answer
+
 def solution(numbers, hand):
     answer = ''
-    left, right = 10, 12    # *을 10, #을 12로 치환
-    for i in range(len(numbers)):
-        if numbers[i] == 0:
-            numbers[i] = 11
+    key_dic = {1: (0, 0), 2: (0, 1), 3: (0, 2),
+               4: (1, 0), 5: (1, 1), 6: (1, 2),
+               7: (2, 0), 8: (2, 1), 9: (2, 2), 
+               0: (3, 1)}
+    left_pos, right_pos = (3, 0), (3, 2)
     
     for n in numbers:
         if n in [1, 4, 7]:
             answer += "L"
-            left = n
+            left_pos = key_dic[n]
         elif n in [3, 6, 9]:
             answer += "R"
-            right = n
-        elif n in [2, 5, 8, 11]:
-            left_dist = (abs(n - left) // 3) + abs(n - left) % 3
-            right_dist = (abs(n - right) // 3) + abs(n - right) % 3
+            right_pos = key_dic[n]
+        elif n in [2, 5, 8, 0]:
+            left_dist = abs(key_dic[n][0] - left_pos[0]) + abs(key_dic[n][1] - left_pos[1])
+            right_dist = abs(key_dic[n][0] - right_pos[0]) + abs(key_dic[n][1] - right_pos[1])
             if left_dist < right_dist:
-                left = n
+                left_pos = key_dic[n]
                 answer += "L"
             elif left_dist > right_dist:
-                right = n
+                right_pos = key_dic[n]
                 answer += "R"
             else:
                 if hand == "left":
-                    left = n
+                    left_pos = key_dic[n]
                     answer += "L"
                 else:
-                    right = n
+                    right_pos = key_dic[n]
                     answer += "R"
                     
-        
-        
     return answer
