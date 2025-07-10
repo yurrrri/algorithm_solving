90ad58786c43d8d4df332d0b43579874ad80afdb
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-07-10 23:03:26 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 292 ms, Memory: 112536 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
@@ -1,5 +1,5 @@
 answer = 0
-dx = [0, 1, 0, -1]
+dx = [0, 1, 0, -1]  # 동, 남, 서, 북 순서
 dy = [1, 0, -1, 0]
 
 r, c, t = map(int, input().split())
@@ -12,40 +12,72 @@ for i in range(r):
       machine.append((i, j))
 
 def spread():
-    temp = [[0]*c for _ in range(r)]
-    
-    for x in range(r):
-      for y in range(c):
-        if board[x][y] > 0:
-          amount = board[x][y] // 5
-          count = 0
-          for dir in range(4):
-            nx, ny = x + dx[dir], y + dy[dir]
-            if 0 <= nx < r and 0 <= ny < c and board[nx][ny] != -1:
-              temp[nx][ny] += amount
-              count += 1
-          board[x][y] -= amount * count
+  temp = [[0] * c for _ in range(r)]
+  
+  for x in range(r):
+    for y in range(c):
+      if board[x][y] > 0:   # 미세먼지가 있는곳
+        count = 0    # 확산된 갯수
+        amount = board[x][y] // 5
+        for dir in range(4):
+          nx = x + dx[dir]
+          ny = y + dy[dir]
 
-    for i in range(r):
-        for j in range(c):
-            board[i][j] += temp[i][j]
+          # 칸이 없거나 (경계를 벗어나거나), 인접한 방향에 공기청정기가 있다면 확산이 일어나지 않는다.
+          if 0<=nx<r and 0<=ny<c and board[nx][ny] != -1:
+            temp[nx][ny] += amount
+            count += 1
 
+        board[x][y] -= amount * count
+        # 먼저 원래 미세먼지의 값을 업데이트해도 이후 미세먼지가 있는 곳에 영향을 주지 않는다. (최대 퍼질 수 있는 곳이 4곳이기 때문이기 때문에, board[x][y]가 0이 될 수 없음)
+
+  for i in range(r):
+    for j in range(c):
+      board[i][j] += temp[i][j]
+  
 def purifying():
-  upper, lower = machine[0][0], machine[1][0]
+  # 위쪽
+  # 동쪽부터시작하니 d=0
+  d = 0
+  before = 0  # 바람으로 밀어내기 전에 이전값을 저장할 변수, 공기청정기에서부터 밀어내니까 처음에는 0임
+  #공기청정기 시작부분 다음칸부터 시작
+  x, y = machine[0][0],machine[0][1]+1
+  
+  while True:
+    # 공기청정기를 만나면 멈춤
+    if x == machine[0][0] and y == machine[0][1]:
+      break
+      
+    nx = x+dx[d]
+    ny = y+dy[d]
 
-  # 위쪽: 반시계 방향
-  for i in range(upper-1, 0, -1): board[i][0] = board[i-1][0]
-  for i in range(c-1): board[0][i] = board[0][i+1]
-  for i in range(upper): board[i][c-1] = board[i+1][c-1]
-  for i in range(c-1, 1, -1): board[upper][i] = board[upper][i-1]
-  board[upper][1] = 0
+    # 그 다음 위치가 바람이 벽을만나면 방향을 꺾음
+    if nx == r or ny == c or nx == -1 or ny == -1:
+      d -= 1
+      continue
+    
+    board[x][y], before = before, board[x][y]   # 바람으로 밀어내고 (교환하고)
+    x,y = nx,ny   # 방향 갱신
 
-  # 아래쪽: 시계 방향
-  for i in range(lower+1, r-1): board[i][0] = board[i+1][0]
-  for i in range(c-1): board[r-1][i] = board[r-1][i+1]
-  for i in range(r-1, lower, -1): board[i][c-1] = board[i-1][c-1]
-  for i in range(c-1, 1, -1): board[lower][i] = board[lower][i-1]
-  board[lower][1] = 0
+  # 아래쪽
+  d=0
+  before=0
+  
+  x,y = machine[1][0],machine[1][1]+1
+  
+  while True:
+    if x == machine[1][0] and y ==machine[1][1]:
+      break
+      
+    nx = x+dx[d]
+    ny = y+dy[d]
+
+    #바람이 벽을만나면 방향을 꺾음
+    if nx == r or ny == c or nx == -1 or ny == -1:
+      d += 1
+      continue
+    board[x][y],before = before,board[x][y]
+    x,y=nx,ny
             
 for _ in range(t):    # t초만큼 반복
   spread()
