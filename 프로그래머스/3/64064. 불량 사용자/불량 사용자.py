d0d471037c0a08e4d479dd227c6a9737e4428bda
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-07-21 11:49:52 +0900

    [level 3] Title: 불량 사용자, Time: 567.49 ms, Memory: 9.16 MB -BaekjoonHub

---
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -0,0 +1,27 @@
+from itertools import permutations
+
+def solution(user_id, banned_id):
+    answer = []
+    
+    def check(_id, standard):
+        if len(_id) != len(standard):
+            return False
+        
+        for u, s in zip(_id, standard):
+            if u != "*" and s != "*" and u != s:  # *가 아닌데 다르면 다른거임
+                return False
+            
+        return True
+    
+    C = permutations(user_id, len(banned_id))
+    for users in C:
+        flag = True
+        for u, b in zip(users, banned_id):
+            if not check(u, b):
+                flag = False
+                break
+        if flag:
+            sorted_list = sorted(list(users))
+            if sorted_list not in answer:
+                answer.append(sorted_list)
+    return len(answer)
