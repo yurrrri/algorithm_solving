97dcee03ef1eb419cc208a3bf82d55e2b06e1090
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-07-25 13:10:23 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 497.72 ms, Memory: 186 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
@@ -1,5 +1,5 @@
 import sys
-sys.setrecursionlimit(10**8)
+sys.setrecursionlimit(10**6)
 
 def solution(land):
     answer = 0
@@ -10,7 +10,6 @@ def solution(land):
     dy = [0, 0, -1, 1]
     num = 0   # 덩어리를 구분짓기 위한 숫자
     land_num = [[0] * m for _ in range(n)]
-    arr = []
     
     def dfs(x, y):
         visited[x][y] = True
@@ -30,19 +29,15 @@ def solution(land):
                 temp = []
                 dfs(i, j)
                 size = len(temp)
-                arr.append([num, size, temp])
-                
-    for num, size, poses in arr:
-        for x, y in poses:
-            land[x][y] = size
-            land_num[x][y] = num
+                for x, y in temp:
+                    land[x][y] = size
+                    land_num[x][y] = num
                                         
     for i in range(m):
         oil_set = set()
         for j in range(n):
             if land[j][i] != 0:
                 oil_set.add((land_num[j][i], land[j][i]))
-        # print(oil_set)
                 
         _sum = 0
         for num, amount in oil_set:
