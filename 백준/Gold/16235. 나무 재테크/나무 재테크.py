4781bad8721a234532a3cbb40bf41123ea7267b4
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-07-26 14:03:40 +0900

    [Gold III] Title: 나무 재테크, Time: 872 ms, Memory: 172224 KB -BaekjoonHub

--- 백준/Gold/16235. 나무 재테크/나무 재테크.py
+++ 백준/Gold/16235. 나무 재테크/나무 재테크.py
@@ -1,13 +1,19 @@
+from collections import deque
+
 n, m, k = map(int, input().split())
 # nxn 크기의 땅
 board = [[5] * n for _ in range(n)]
 A = []  # 겨울에 추가되는 양분의 양
-trees = [[[] for _ in range(n)] for _ in range(n)]  # 각 칸마다의 나무 나이, 여러 개의 나무가 있을 수도 있다
+trees = [[deque() for _ in range(n)] for _ in range(n)]  # 각 칸마다의 나무 나이, 여러 개의 나무가 있을 수도 있다
 for _ in range(n):
   A.append(list(map(int, input().split())))
 for _ in range(m):
   x, y, z = map(int, input().split())
   trees[x-1][y-1].append((z, False))
+for i in range(n):
+  for j in range(n):
+    if trees[i][j]:
+      trees[i][j] = deque(sorted(trees[i][j]))
 
 dx = [-1, -1, -1, 0, 0, 1, 1, 1]
 dy = [-1, 0, 1, -1, 1, -1, 0, 1]
@@ -17,7 +23,7 @@ for _ in range(k):
   for i in range(n):
     for j in range(n):
       if trees[i][j]:
-        temp = []
+        temp = deque([])
         for age, isDead in trees[i][j]:    # 나이가 어린 나무부터 양분을 먹기 위해 정렬
           if not isDead:
             if board[i][j] < age: # 양분 부족
@@ -47,7 +53,7 @@ for _ in range(k):
               ny = y + dy[l]
 
               if 0<=nx<n and 0<=ny<n:
-                trees[nx][ny].insert(0, (1, False))
+                trees[nx][ny].appendleft((1, False))
   
   # 겨울
   for i in range(n):
