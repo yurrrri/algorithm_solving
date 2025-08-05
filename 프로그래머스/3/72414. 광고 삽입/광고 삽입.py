c7d413b1939fc70beea977cc5cf29e785e7486cd
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-08-05 15:35:10 +0900

    [level 3] Title: 광고 삽입, Time: 900.88 ms, Memory: 51.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/72414. 광고 삽입/광고 삽입.py
@@ -0,0 +1,47 @@
+def transform_time(time):
+    hour, minute, second = map(int, time.split(":"))
+    
+    return 3600 * hour + 60 * minute + second
+
+def reverse_time(time):
+    h = time // 3600
+    time %= 3600
+    m = time // 60
+    time %= 60
+    s = time
+    return f"{h:02d}:{m:02d}:{s:02d}"
+
+def solution(play_time, adv_time, logs):
+    if play_time == adv_time:
+        return "00:00:00"
+    play_time = transform_time(play_time)
+    adv_time = transform_time(adv_time)
+    
+    timeline = [0] * (play_time + 2)  # 마지막 범위 처리를 위해 +2
+
+    # 누적 시청자 수 기록
+    for log in logs:
+        start, end = log.split("-")
+        start_sec = transform_time(start)
+        end_sec = transform_time(end)
+        timeline[start_sec] += 1
+        timeline[end_sec] -= 1
+
+    # prefix sum 1: 시각별 시청자 수
+    for i in range(1, play_time + 1):
+        timeline[i] += timeline[i - 1]
+
+    # prefix sum 2: 시각별 누적 시청시간
+    for i in range(1, play_time + 1):
+        timeline[i] += timeline[i - 1]
+
+    max_time = 0
+    answer = 0
+    
+    for t in range(play_time - adv_time + 1):
+        prefix_sum = timeline[t + adv_time - 1] - timeline[t - 1]
+        if prefix_sum > max_time:
+            max_time = prefix_sum
+            answer = t
+
+    return reverse_time(answer)
