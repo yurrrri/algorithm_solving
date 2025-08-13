c6a434e2cb714da5e9a3c28e1b61dca67b4cc2b2
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-08-13 23:36:41 +0900

    [Gold IV] Title: 드래곤 앤 던전, Time: 448 ms, Memory: 129244 KB -BaekjoonHub

---
+++ 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.py
@@ -0,0 +1,40 @@
+n, p = map(int, input().split())  # 방의 개수, 초기 공격력
+arr = []
+for _ in range(n):
+  t, a, h = map(int, input().split())
+  arr.append((t, a, h))
+  # t가 1이면 공격력 a, 생명력 h인 몬스터
+  # t가 2이면 공격력 a, 생명력 h증가 포션
+
+def can_clear(atk, maxhp):
+  current_hp = maxhp
+  
+  for t, a, h in arr:
+    if t == 2:
+      atk += a
+      current_hp += h
+      current_hp = min(current_hp, maxhp)   # 용사 생명력이 최대 생명력(maxhp)를 넘으면 maxhp
+    else:
+      if h%atk==0:
+        current_hp -= ((h//atk)-1)*a
+      else:
+        current_hp -= (h//atk)*a
+  
+      if current_hp <= 0:
+        return False
+  
+  return True
+
+start, end = 1, n*int(1e12)
+answer = 0
+
+while start <= end:
+  mid = (start + end) // 2
+
+  if can_clear(p, mid):
+    answer = mid
+    end = mid - 1   # 범위를 더 낮춰보기
+  else:
+    start = mid + 1
+
+print(answer)
