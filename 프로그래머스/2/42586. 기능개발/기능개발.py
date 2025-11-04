40e7a43dcb0e9ecf9620c0eb3f198000690738f4
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-11-04 21:28:42 +0900

    [level 2] Title: 기능개발, Time: 0.46 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -8,11 +8,11 @@ def solution(progresses, speeds):
     while progresses:
         count = 0
         for i in range(len(progresses)):
-            progresses[i] += speeds[i]
-        while progresses and progresses[0] >= 100:
+            progresses[i] += speeds[i]  
+        while progresses and progresses[0] >= 100:  # 앞의 기능이 진도가 100% 이상일때까지 같이 popleft함 (하루에 같이 배포하기 위해)
             count += 1
             progresses.popleft()
-            speeds.popleft()
+            speeds.popleft()    # speeds를 함께 popleft하는 이유는, 위 10번째 라인에서 progresses만 pop하게 되면 인덱스가 안맞아서 각 공정에 맞는 스피드로 계산할 수가 없게된다.
         if count:
             answer.append(count)
     return answer
