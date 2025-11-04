47897c67bc57bb3982c96c521bd478f979eb5b6d
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-11-04 20:18:16 +0900

    [level 2] Title: 괄호 회전하기, Time: 108.02 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,22 +1,25 @@
-def isCorrectGwalho(s):
+def ispair(string):
     stack = []
-    for c in s:
-        if c in ["(", "[", "{"]:
-            stack.append(c)
+    
+    for s in string:
+        if s in ["[", "(", "{"]:
+            stack.append(s)
         else:
-            if len(stack) == 0:   # 닫는 괄호를 만나는 시점에서 스택이 비어있다면 이미 올바른 문자열이 아니므로 early return 처리한다.
+            if not stack:
                 return False
+            if stack[-1] == "[" and s == "]" or stack[-1] == "(" and s == ")" or stack[-1] == "{" and s == "}":
+                stack.pop()
             
-            if (stack[-1] == "(" and c == ")") or (stack[-1] == "[" and c == "]") or (stack[-1] == "{" and c == "}"):
-                stack.pop() 
-                
-    return len(stack) == 0   # 스택이 비어있음 -> 즉, 모든 괄호가 상쇄되므로 return true
+    return len(stack) == 0
+    
 
 def solution(s):
     answer = 0
     s = list(s)
+    
     for _ in range(len(s)):
-        s += s.pop(0)
-        if isCorrectGwalho(s):
+        s.insert(0, s.pop())
+        if ispair(s):
             answer += 1
+    
     return answer
