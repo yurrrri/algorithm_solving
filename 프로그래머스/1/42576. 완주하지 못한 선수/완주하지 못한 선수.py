e1e4c2c932744459dc524a456dba1f31a9f8a16c
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-11-04 21:43:07 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 88.88 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -2,5 +2,6 @@ from collections import Counter
 
 def solution(participant, completion):
     counter = Counter(participant) - Counter(completion)
+    # counter 집합을 차집합 연산했을 때, value가 0인 경우 해당 개체가 제거된다.
         
     return list(counter.keys())[-1]
