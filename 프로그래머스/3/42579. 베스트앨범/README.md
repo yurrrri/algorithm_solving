8f179a73fd499a9449ea673ee3979f91b608560f
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-11-06 16:38:57 +0900

    [level 3] Title: 베스트앨범, Time: 0.10 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/README.md
+++ 프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=python3#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
 
 ### 성능 요약
 
-메모리: 9.23 MB, 시간: 0.07 ms
+메모리: 9.31 MB, 시간: 0.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 19일 15:59:01
+2025년 11월 06일 16:38:55
 
 ### 문제 설명
 
--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -2,15 +2,15 @@ from collections import defaultdict
 
 def solution(genres, plays):
     answer = []
-    play_total_dic = defaultdict(int)      # 장르 별 총 재생횟수를 저장할 딕셔너리
-    genre_play_dic = defaultdict(list)      # 장르별 (고유번호, 재생횟수) 튜플을 저장할 딕셔너리
-    for i, (genre, play) in enumerate(zip(genres, plays)):
-        play_total_dic[genre] += play
-        genre_play_dic[genre].append((i, play))
-        
-    sorted_play_total = sorted(play_total_dic, key=lambda x:-play_total_dic[x])
+    genre_total_playtimes = defaultdict(int)
+    genre_playtimes = defaultdict(list)
     
-    for genre in sorted_play_total:
-        answer.extend([i for i, _ in sorted(genre_play_dic[genre], key=lambda x:(-x[1], x[0]))[:2]])
-
+    for i, (g, p) in enumerate(zip(genres, plays)):    # 장르별 가장 많이 재생된 노래를 찾기 위한 딕셔너리
+        genre_total_playtimes[g] += p
+        genre_playtimes[g].append((i, p))     # 장르별 노래의 고유번호, 플레이횟수
+        
+    for genre in sorted(genre_total_playtimes, key=lambda x:-genre_total_playtimes[x]):   # 장르별 총 재생횟수를 내림차순으로 정렬한 키값 리스트
+        answer.extend([i for i, _ in sorted(genre_playtimes[genre], key=lambda x:(-x[1], x[0]))][:2])
+    
+    
     return answer
