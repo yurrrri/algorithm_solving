b9ef9688d509d3d3f98159e087cfed7ab63e8726
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-11-06 15:32:34 +0900

    [level 2] Title: 할인 행사, Time: 215.95 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -3,12 +3,13 @@ from collections import Counter
 def solution(want, number, discount):
     answer = 0
     want_dic = {}
-    for w, n in zip(want, number):
-        want_dic[w] = n
+    
+    for p, n in zip(want, number):
+        want_dic[p] = n
         
-    for i in range(len(discount)):
+    for i in range(len(discount)-9):
         counter = Counter(discount[i:i+10])
-        if want_dic == counter:
+        if counter == want_dic:
             answer += 1
-        
+    
     return answer
