640534db1cd80982f3ab85c1680fba07bd83a310
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-11-06 17:18:40 +0900

    [level 1] Title: 신고 결과 받기, Time: 219.87 ms, Memory: 62.6 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -2,24 +2,20 @@ from collections import defaultdict
 
 def solution(id_list, report, k):
     answer = []
-    
-    report_dic = defaultdict(set)
-    reported_dic = {x: 0 for x in id_list}
+    report_history = defaultdict(set)
+    reported_history = defaultdict(set)
     
     for r in report:
-        user, reported = r.split()
-        report_dic[user].add(reported)
+        reporter, reported = r.split()
+        report_history[reporter].add(reported)
+        reported_history[reported].add(reporter)
         
-    for _id in id_list:
-        for reported in report_dic[_id]:
-            reported_dic[reported] += 1
-            
-    for _id in id_list:
-        _sum = 0
-        for reported in report_dic[_id]:
-            if reported_dic[reported] >= k:
-                _sum += 1
+    for id in id_list:
+        count = 0
+        for reported in report_history[id]:
+            if len(reported_history[reported]) >= k:
+                count += 1
                 
-        answer.append(_sum)
+        answer.append(count)
     
     return answer
