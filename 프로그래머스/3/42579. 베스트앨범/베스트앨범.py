996ea3b28a3fe1c7c7a605a3b717d41d2fe8fa81
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-11-06 16:45:43 +0900

    [level 3] Title: 베스트앨범, Time: 0.12 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -10,7 +10,7 @@ def solution(genres, plays):
         genre_playtimes[g].append((i, p))     # 장르별 노래의 고유번호, 플레이횟수
         
     for genre in sorted(genre_total_playtimes, key=lambda x:-genre_total_playtimes[x]):   # 장르별 총 재생횟수를 내림차순으로 정렬한 키값 리스트
-        answer.extend([i for i, _ in sorted(genre_playtimes[genre], key=lambda x:(-x[1], x[0]))][:2])
+        answer += [i for i, _ in sorted(genre_playtimes[genre], key=lambda x:(-x[1], x[0]))][:2]
     
     
     return answer
