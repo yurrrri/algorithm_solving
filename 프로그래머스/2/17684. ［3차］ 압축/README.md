310eaf0d8241d3ae8bc89af30b982b9e3a5876a2
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-11-07 19:38:10 +0900

    [level 2] Title: [3차] 압축, Time: 0.46 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/README.md
+++ 프로그래머스/2/17684. ［3차］ 압축/README.md
@@ -1,10 +1,10 @@
 # [level 2] [3차] 압축 - 17684 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17684?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17684) 
 
 ### 성능 요약
 
-메모리: 9.26 MB, 시간: 0.65 ms
+메모리: 9.12 MB, 시간: 0.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 19일 20:14:17
+2025년 11월 07일 19:38:08
 
 ### 문제 설명
 
--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
@@ -3,16 +3,19 @@ from collections import deque
 def solution(msg):
     answer = []
     dic = {}
-    for i in range(1, 27):          # 색인 사전
-        dic[chr(ord('A') + i - 1)] = i
+    for i in range(26):
+        dic[chr(ord('A')+i)] = i+1   # 1. 길이가 1인 모든 단어를 포함하도록 사전을 초기화한다.
+    msg = deque(msg)
+    number = 26
         
-    queue = deque(msg)
-    while queue:
-        temp = queue.popleft()
-        while queue and temp + queue[0] in dic:
-            temp += queue.popleft()
-        answer.append(dic[temp])
-        if queue:
-            dic[temp+queue[0]] = len(dic) + 1
-            
+    while msg:
+        poped = msg.popleft()
+        
+        while msg and poped + msg[0] in dic:  # 2. 사전에서현재 입력과 일치하는 가장 긴 문자열 w를 찾는다
+            poped += msg.popleft()
+        answer.append(dic[poped])    # 3. 색인번호 출력 후, 입력에서 w 제거
+        
+        if msg:     # 4. 처리되지 않은 다음 글자가 남아있다면, 
+            number += 1
+            dic[poped + msg[0]] = number    # w+c에 해당하는 단어를 사전에 등록한다. (이때 제거하는게 아니므로 pop하지 말것)
     return answer
