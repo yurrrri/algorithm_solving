224d365eb6359efd03404c4f48e5605d549eaaf8
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-11-18 21:42:36 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 31.05 ms, Memory: 9.57 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -5,27 +5,21 @@ def compare(a, b):
     axis = 0    # 기준 1, 2에서 모두 비교할 수 없을 경우 원래 입력에 주어진 순서를 유지하므로, 초기값을 0으로 세팅한다.
     
     def extract_from_file(file):
-
-        head, number, tail = ("", "", "")
         number_start_idx = 0
-
-        for idx, f in enumerate(file):
-            if f.isdigit():
-                head = file[:idx]
-                number_start_idx = idx
+        tail_start_idx = len(file)
+        
+        for i, s in enumerate(file):
+            if s.isdigit():
+                number_start_idx = i
                 break
                 
-        for i in range(number_start_idx, len(file)):
-            if not file[i].isdigit():
-                number = file[number_start_idx:i]
-                tail = file[i:]
+        for j in range(number_start_idx, len(file)):
+            if not file[j].isdigit():
+                tail_start_idx = j
                 break
                 
-        if len(number) == 0:
-            number = file[number_start_idx:]
-
-        return head, number, tail
-    
+        return file[:number_start_idx], file[number_start_idx:tail_start_idx], file[tail_start_idx:]
+        
     a_head, a_number, a_tail = extract_from_file(a)
     b_head, b_number, b_tail = extract_from_file(b)
     
@@ -40,7 +34,7 @@ def compare(a, b):
             axis = 1
         else:
             axis = 0
-        
+            
     return axis
 
 def solution(files):
