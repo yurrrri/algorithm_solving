ffd3e3f7c242986d1da248d2e7aca7e52dd93891
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-11-18 21:13:53 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 38.72 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/README.md
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/README.md
@@ -1,10 +1,10 @@
 # [level 3] [카카오 인턴] 경주로 건설 - 67259 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67259?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67259) 
 
 ### 성능 요약
 
-메모리: 9.41 MB, 시간: 42.35 ms
+메모리: 9.26 MB, 시간: 38.72 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 23일 21:35:22
+2025년 11월 18일 21:13:50
 
 ### 문제 설명
 
--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -8,21 +8,22 @@ def solution(board):
     INF = int(1e9)
     visited = [[[INF] * 4 for _ in range(n)] for _ in range(n)]
     q = deque()
-    for i in range(4):
+    for i in range(4):         # 처음의 모든 방향을 0으로 초기화
         q.append((0, 0, i, 0))
         visited[0][0][i] = 0
     
-    def is_valid(x, y):
+    def is_valid(x, y):     # 도로를 설치할 수 있는지 판단해주는 함수
         if 0<=x<n and 0<=y<n and board[x][y] == 0:
             return True
         return False
     
     def calculate_cost(prev, current):
-        if prev == -1 or abs(prev-current) % 2 == 0:
+        if abs(prev-current) % 2 == 0:   # 상-하, 좌-우 등 같은 방향일 경우의 직선 도로 비용
             return 100
-        else:
+        else:    # 다를 경우 코너로 꺾어야하므로 코너 비용 100 + 500
             return 600
         
+    # BFS 실행시, 최소 비용을 계산할때 방문시마다 어떤 정보가 필요한지를 판단하고 큐와 visited 배열을 설계해야한다.
     while q:
         x, y, prev_dir, cost = q.popleft()
 
@@ -32,7 +33,7 @@ def solution(board):
             
             new_cost = cost + calculate_cost(prev_dir, i)
             
-            if is_valid(nx, ny) and visited[nx][ny][i] > new_cost:
+            if is_valid(nx, ny) and visited[nx][ny][i] > new_cost:    # 방문한 적이 없거나(INF), new_cost가 기존 비용보다 더 작으면 이걸로 갱신
                 visited[nx][ny][i] = new_cost
                 q.append((nx, ny, i, new_cost))
     
