cefd076679088e61b2c820a75982cf1c235775c7
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-11-26 19:13:23 +0900

    [level 2] Title: 땅따먹기, Time: 226.68 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -4,10 +4,7 @@ def solution(land):
     
     for i in range(1, n):
         for j in range(4):
-            temp = 0
-            for k in range(4):
-                if j != k:
-                    temp = max(land[i-1][k], temp)
+            temp = max(land[i-1][:j] + land[i-1][j+1:])
             land[i][j] += temp
 
     return max(land[-1])
