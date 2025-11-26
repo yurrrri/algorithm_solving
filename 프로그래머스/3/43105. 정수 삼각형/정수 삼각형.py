f39228d20a76624b6d248080026b268e72ee85c8
Lee Yuri <leeyuri.iosapp@gmail.com>
2025-11-26 18:41:02 +0900

    [level 3] Title: 정수 삼각형, Time: 51.67 ms, Memory: 19.2 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -8,12 +8,12 @@ def solution(triangle):
     dp[1][0] = dp[0][0] + triangle[1][0]
     dp[1][1] = dp[0][0] + triangle[1][1]
     
-    for i in range(2, n):
+    for i in range(2, n):      # 점화식 세우기
         for j in range(i+1):
             if j == 0:
-                dp[i][0] = dp[i-1][0] + triangle[i][0]
+                dp[i][0] = dp[i-1][0] + triangle[i][0]   # 왼쪽은 왼쪽에서만 내려옴
             elif j == i:
-                dp[i][j] = dp[i-1][j-1] + triangle[i][j]
+                dp[i][j] = dp[i-1][j-1] + triangle[i][j]   # 오른쪽은 오른쪽에서만 내려옴
             else:
-                dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j]
+                dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j]  # 이전 dp의 왼쪽, 오른쪽 비교
     return max(dp[n-1])
