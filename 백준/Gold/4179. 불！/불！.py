288e014e54393dbfd05c0b8bde65abe5635fbdf9
Lee Yuri <misslee8403@gmail.com>
2025-12-17 12:21:09 +0900

    [Gold III] Title: 불!, Time: 332 ms, Memory: 139940 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.py
+++ 백준/Gold/4179. 불！/불！.py
@@ -2,59 +2,59 @@ from collections import deque
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-
-r, c = map(int, input().split())
 board = []
-fire_q = deque([])
-fire_visited = [[0] * c for _ in range(r)]
+
+R, C = map(int, input().split())
+visited_f = [[-1] * C for _ in range(R)]
+visited = [[False] * C for _ in range(R)]
+
+fire_x, fire_y = 0, 0
+jihoon_x, jihoon_y = 0, 0
 q = deque([])
-visited = [[0] * c for _ in range(r)]
 
-for i in range(r):
+for i in range(R):
   board.append(input())
-  for j in range(c):
+  for j in range(C):
     if board[i][j] == "J":
-      q.append((i, j))
-      visited[i][j] = 1
+      jihoon_x, jihoon_y = i, j
     elif board[i][j] == "F":
-      fire_q.append((i, j))
-      fire_visited[i][j] = 1
+      q.append((i, j))
 
-answer = 0
+# 불 먼저 이동
+for x, y in q:
+  visited_f[x][y] = 0
 
-def fire_bfs():
-  while fire_q:
-    x, y = fire_q.popleft()
+while q:
+  x, y = q.popleft()
 
-    for i in range(4):
-      nx = x + dx[i]
-      ny = y + dy[i]
+  for i in range(4):
+    nx = x + dx[i]
+    ny = y + dy[i]
 
-      if 0<=nx<r and 0<=ny<c and board[nx][ny] != "#" and not fire_visited[nx][ny]:
-        fire_visited[nx][ny] = fire_visited[x][y] + 1
-        fire_q.append((nx, ny))
+    if not (0<=nx<R and 0<=ny<C):
+      continue
 
-def bfs():
-  while q:
-    x, y = q.popleft()
+    if board[nx][ny] != "#" and visited_f[nx][ny] == -1:
+      visited_f[nx][ny] = visited_f[x][y] + 1
+      q.append((nx, ny))
 
-    for i in range(4):
-      nx = x + dx[i]
-      ny = y + dy[i]
+# 지훈 이동
+visited[jihoon_x][jihoon_y] = True
+q = deque([(jihoon_x, jihoon_y, 0)])
 
-      if 0<=nx<r and 0<=ny<c and board[nx][ny] == "." and not visited[nx][ny]:
-        if (not fire_visited[nx][ny] or fire_visited[nx][ny] > visited[x][y] + 1):
-          visited[nx][ny] = visited[x][y] + 1
-          q.append((nx, ny))
-        
-      elif not(0<=nx<r and 0<=ny<c):  # 범위를 벗어나면 탈출할 수 있다는 의미이므로 거리 출력하고 break
-        return visited[x][y]
+while q:
+  x, y, t = q.popleft()
+    
+  for i in range(4):
+    nx = x + dx[i]
+    ny = y + dy[i]
 
-  return -1
+    if 0<=nx<R and 0<=ny<C:
+      if board[nx][ny] == "." and not visited[nx][ny] and (visited_f[nx][ny] == -1 or t + 1 < visited_f[nx][ny]):
+        visited[nx][ny] = True
+        q.append((nx, ny, t+1))
+    else:
+      print(t+1)
+      exit()
 
-fire_bfs()
-result = bfs()
-if result == -1:
-  print("IMPOSSIBLE")
-else:
-  print(result)
+print("IMPOSSIBLE")
